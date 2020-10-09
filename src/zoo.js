3239295a5ddb3b6d8dc2407e8fd669c51de3c5bd
danielmlduarte <danielduarte.tryber@gmail.com>
2020-10-09 19:16:38 -0300

    Resoluçao do quinto requisito - isManager

--- src/zoo.js
+++ src/zoo.js
@@ -51,7 +51,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  return data.employees.some(employee => employee.managers.some(manager => manager === id));
+  return data.employees.some( employee => employee.managers.some(manager => manager === id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
