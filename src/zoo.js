3cb159ce81974ce425df4583e06ad67df2860da3
Muniz <vinicius.muniz.marin@gmail.com>
2020-10-09 16:57:36 -0300

    6. function addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -47,8 +47,21 @@ function isManager(id) {
   return result;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = [],
+) {
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  data.employees.push(newEmployee);
 }
 
 function animalCount(species) {
