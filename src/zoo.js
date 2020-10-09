3be96559f2842bbcfab0889eff9fcc0d8e9aa3dc
Lucas Andrade <lafdesouza2002@outlook.com>
2020-10-09 18:01:33 -0300

    Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -37,8 +37,14 @@ function isManager(id) {
   // seu código aqui
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+ return data.employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor
+  });
 }
 
 function animalCount(species) {
