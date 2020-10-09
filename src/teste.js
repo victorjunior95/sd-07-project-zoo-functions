b1c20f736983eaf9e4fa58450dc9b18176ae0dfd
renatomak <renato.mark.silva@gmail.com>
2020-10-09 17:23:27 -0300

    primeiro commit - requisito 1

---
+++ src/teste.js
@@ -0,0 +1,78 @@
+const array = [
+  [{
+    "id": "0938aa23-f153-4937-9f88-4858b24d6bce",
+    "location": "NE",
+    "name": "lions",
+    "popularity": 4,
+    "residents": [{
+      "age": 12,
+      "name": "Zena",
+      "sex": "female"
+    }, {
+      "age": 15,
+      "name": "Maxwell",
+      "sex": "male"
+    }, {
+      "age": 7,
+      "name": "Faustino",
+      "sex": "male"
+    }, {
+      "age": 14,
+      "name": "Dee",
+      "sex": "female"
+    }]
+  }],
+  [{
+    "id": "e8481c1d-42ea-4610-8e11-1752cfc05a46",
+    "location": "NW",
+    "name": "tigers",
+    "popularity": 5,
+    "residents": [{
+      "age": 19,
+      "name": "Shu",
+      "sex": "female"
+    }, {
+      "age": 17,
+      "name": "Esther",
+      "sex": "female"
+    }]
+  }]
+];
+
+const novoArray = [{
+  "id": "0938aa23-f153-4937-9f88-4858b24d6bce",
+  "location": "NE",
+  "name": "lions",
+  "popularity": 4,
+  "residents": [{
+    "age": 12,
+    "name": "Zena",
+    "sex": "female"
+  }, {
+    "age": 15,
+    "name": "Maxwell",
+    "sex": "male"
+  }, {
+    "age": 7,
+    "name": "Faustino",
+    "sex": "male"
+  }, {
+    "age": 14,
+    "name": "Dee",
+    "sex": "female"
+  }]
+}, {
+  "id": "e8481c1d-42ea-4610-8e11-1752cfc05a46",
+  "location": "NW",
+  "name": "tigers",
+  "popularity": 5,
+  "residents": [{
+    "age": 19,
+    "name": "Shu",
+    "sex": "female"
+  }, {
+    "age": 17,
+    "name": "Esther",
+    "sex": "female"
+  }]
+}]
