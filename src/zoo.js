67e419d6887bb4ffb4ad90c256c318ded27b671d
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-10-09 14:59:43 -0300

    requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,14 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  if (ids.length === 0) return [];
+
+  const allByIds = data.animals.filter(({id: animalId}) => {
+    return ids.find((id) => id === animalId);
+  });
+
+  return allByIds;
 }
 
 function animalsOlderThan(animal, age) {
