f5c6c233ff9141b403a4b06999132e8ad2cfef9f
vladimirrchagas <vladimirrchagas@gmail.com>
2020-10-09 16:47:48 -0300

    Ajuste de CC

--- src/zoo.js
+++ src/zoo.js
@@ -45,15 +45,12 @@ function animalsOlderThan(animal, age) {
 
 function employeeByName(employeeName) {
   // seu código aqui
-  
   if (employeeName === undefined) {
     return {};
   }
-
-  const employeeObject = data.employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
-
-  return employeeObject;
+  const employeeObject = data.employees.find(element => element.firstName === employeeName || element.lastName === employeeName);
   
+  return employeeObject;
 }
 
 function createEmployee(personalInfo, associatedWith) {
