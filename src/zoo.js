10a75ceebd1d2d5e23cef4d680f8f69159e4fbee
vanderson-henrique <vhbenedito@gmail.com>
2020-10-09 17:31:36 -0300

    Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -39,8 +39,8 @@ function isManager(id) {
   return data.employees.some(employee => employee.managers.some(idManager => idManager === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
