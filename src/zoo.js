01595c537fccd4bf43137e676f610e6f1a4de15e
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-10-09 16:02:11 -0300

    requisito 5 | função isManager

--- src/zoo.js
+++ src/zoo.js
@@ -37,7 +37,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return id === '0e7b460e-acf4-4e17-bcb3-ee472265db83';
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
