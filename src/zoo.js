033505500346db848db6375a630c89cec876f201
Carol Bezerra <carol.arbe@gmail.com>
2020-10-09 18:14:47 -0300

    Requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -13,18 +13,17 @@ const data = require('./data');
 const { animals } = require('./data');
 
 function animalsByIds(...ids) {
-  // console.log(...ids);
-  console.log(ids);
-  const idAnimal = animals.filter((justId) => justId.id === ids);
+  const idAnimal = animals.filter(justId => ids.includes(justId.id));
+  return idAnimal;
+} // feito com a ajuda do Murilo Wolf no plantão
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/contains
 
-  return idAnimal
-}
-
-animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  // const olders = animals.every((element, index) =>
+  //   element.name === animal && element.residents[index].age >= age)
+  // return olders;
 }
-
+// console.log(animalsOlderThan('otters', 7))
 function employeeByName(employeeName) {
   // seu código aqui
 }
