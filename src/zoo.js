ec7ac7bbf3c9865167e81cccff02c8c0c6c83a25
marcelosoliveira <msbobsk8@gmail.com>
2020-10-09 18:00:25 -0300

    Requisito 6 ok

--- src/zoo.js
+++ src/zoo.js
@@ -41,8 +41,8 @@ function isManager(id) {
   return data.employees.some(manger => manger.id === id && manger.managers.length === 1);
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return data.employees.push({id, firstName, lastName, managers, responsibleFor});
 }
 
 function animalCount(species) {
