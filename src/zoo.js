5da52f4cb270254a2386681a02ab95b3cac99815
Lfocosi <luccafocosi@gmail.com>
2020-10-09 18:01:50 -0300

    scan de ids

--- src/zoo.js
+++ src/zoo.js
@@ -24,8 +24,6 @@ function animalsByIds(...ids) {
   }
   return scanId;
 }
-console.log(animalsByIds());
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
