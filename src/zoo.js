5654c7851156258e1e72da8b4ce8739316fc0b83
Carol Bezerra <carol.arbe@gmail.com>
2020-10-09 17:15:17 -0300

    Initial Commit

--- src/zoo.js
+++ src/zoo.js
@@ -10,11 +10,17 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { animals } = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  // console.log(...ids);
+  console.log(ids);
+  const idAnimal = animals.filter((justId) => justId.id === ids);
+
+  return idAnimal
 }
 
+animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
 function animalsOlderThan(animal, age) {
   // seu código aqui
 }
