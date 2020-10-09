c19fbf10d15a285a4ab06d1a58cf25bcf6dcf818
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-10-09 15:46:37 -0300

    requisito 4 | função createEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -33,7 +33,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return Object.assign(personalInfo, associatedWith);
 }
 
 function isManager(id) {
