0b35ab7fe0a26bb74852eafad9a1d233f6947a0b
vanderson-henrique <vhbenedito@gmail.com>
2020-10-09 16:13:48 -0300

    Requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,9 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui...
+function animalsByIds(...ids) {
+  if (ids.length === 0) return [];
+  return data.animals.filter(animal => ids.some(id => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
