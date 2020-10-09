e838b9ba3d10d70df2edd658f0c469475389b60f
Cainan Coutinho <cainan6697@gmail.com>
2020-10-09 16:32:21 -0300

    Primeiro requisito, animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,8 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqu
+function animalsByIds(...ids) {
+  return ids.map(id => data.animals.find(animal => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
