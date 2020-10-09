fc6f9fb34f93da757399c62d55e3d590acc5f5ef
cezar88augusto <cezar88augusto@gmail.com>
2020-10-09 17:45:32 -0300

    Requisito 2 e 7

--- src/zoo.js
+++ src/zoo.js
@@ -9,22 +9,30 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  let bool = true;
+  const resp = animals.find(animalElement => animalElement.name === animal);
+  resp.residents.forEach((element) => {
+    if (element.age < age) {
+      bool = false;
+    }
+  });
+  return bool;
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+
 }
 
 function isManager(id) {
@@ -35,8 +43,21 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
 
-function animalCount(species) {
-  // seu código aqui
+function animalCount(species = '') {
+  if (species !== '') {
+    return animals.find(animal => animal.name === species).residents.length;
+  }
+  return {
+    lions: 4,
+    tigers: 2,
+    bears: 3,
+    penguins: 4,
+    otters: 4,
+    frogs: 2,
+    snakes: 2,
+    elephants: 4,
+    giraffes: 6,
+  };
 }
 
 function entryCalculator(entrants) {
