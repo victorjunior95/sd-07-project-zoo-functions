e868b60be8192e247ad0784934be1f744c957369
Fernando <nando.neves@gmail.com>
2020-10-09 15:10:53 -0300

    codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -11,19 +11,22 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(...ids) { //id entra como string
-  // console.log("ids vale" +ids)
+function animalsByIds(...ids) {
   // seu código aqui
-  let result =[];
-  // console.log('todosids vale' +ids)
-    if (ids !== undefined){
-    for (i in ids){
-      data.animals.filter ((animal) =>{if (animal.id === ids[i]){result.push(animal); return true;}})
+  const result = [];
+  if (ids !== undefined) {
+    for (let i = 0; i < ids.length; i += 1) {
+      data.animals.filter((animal) => {
+        if (animal.id === ids[i]) {
+          result.push(animal);
+          return true;
         }
-      }
-    return result;
+        return false;
+      });
+    }
+  }
+  return result;
 }
-console.log (animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 function animalsOlderThan(animal, age) {
   // seu código aqui
 }
