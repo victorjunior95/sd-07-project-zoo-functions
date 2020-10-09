00fc63d461419d2a05dc3ecbba3fd9df762b3dd0
Andre Menezes <dehhmenezes87@gmail.com>
2020-10-09 20:08:46 -0300

    Requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -1,56 +1,80 @@
-const assert = require('assert');
-const zoo = require('../src/zoo');
+/*
+eslint no-unused-vars: [
+  "error",
+  {
+    "args": "none",
+    "vars": "local",
+    "varsIgnorePattern": "data"
+  }
+]
+*/
 
-describe('Implemente a função animalsByIds', () => {
-  it('Caso receba nenhum parâmetro, necessário retornar um array vazio', () => {
-    const actual = zoo.animalsByIds();
-    const expected = [];
-    assert.deepEqual(actual, expected);
-  });
+const data = require('./data');
 
-  it('Ao receber como parâmetro um único id, retorna os animais com este id', () => {
-    const actual = zoo.animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
-    const expected = [{
-      id: '0938aa23-f153-4937-9f88-4858b24d6bce',
-      name: 'lions',
-      popularity: 4,
-      location: 'NE',
-      residents: [
-        { name: 'Zena', sex: 'female', age: 12 },
-        { name: 'Maxwell', sex: 'male', age: 15 },
-        { name: 'Faustino', sex: 'male', age: 7 },
-        { name: 'Dee', sex: 'female', age: 14 }
-      ]
-    }]
+function animalsByIds(...ids) {
+  return data.animals.filter(animal => ids.includes(animal.id));
+}
 
-    assert.deepEqual(actual, expected);
-  });
+function animalsOlderThan(animal, age) {
+  // seu código aqui
+}
 
-  it('Ao receber mais de um id, retorna os animais que têm um desses ids', () => {
-    const actual = zoo.animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
-    const expected = [{
-      id: '0938aa23-f153-4937-9f88-4858b24d6bce',
-      name: 'lions',
-      popularity: 4,
-      location: 'NE',
-      residents: [
-        { name: 'Zena', sex: 'female', age: 12 },
-        { name: 'Maxwell', sex: 'male', age: 15 },
-        { name: 'Faustino', sex: 'male', age: 7 },
-        { name: 'Dee', sex: 'female', age: 14 }
-      ]
-    },
-    {
-      id: 'e8481c1d-42ea-4610-8e11-1752cfc05a46',
-      name: 'tigers',
-      popularity: 5,
-      location: 'NW',
-      residents: [
-        { name: 'Shu', sex: 'female', age: 19 },
-        { name: 'Esther', sex: 'female', age: 17 }
-      ]
-    }];
+function employeeByName(employeeName) {
+  // seu código aqui
+}
 
-    assert.deepEqual(actual, expected);
-  });
-});
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+}
+
+function isManager(id) {
+  // seu código aqui
+}
+
+function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  // seu código aqui
+}
+
+function animalCount(species) {
+  // seu código aqui
+}
+
+function entryCalculator(entrants) {
+  // seu código aqui
+}
+
+function animalMap(options) {
+  // seu código aqui
+}
+
+function schedule(dayName) {
+  // seu código aqui
+}
+
+function oldestFromFirstSpecies(id) {
+  // seu código aqui
+}
+
+function increasePrices(percentage) {
+  // seu código aqui
+}
+
+function employeeCoverage(idOrName) {
+  // seu código aqui
+}
+
+module.exports = {
+  entryCalculator,
+  schedule,
+  animalCount,
+  animalMap,
+  animalsByIds,
+  employeeByName,
+  employeeCoverage,
+  addEmployee,
+  isManager,
+  animalsOlderThan,
+  oldestFromFirstSpecies,
+  increasePrices,
+  createEmployee,
+};
