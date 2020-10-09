639d411bb016689693656b2c914646c008535949
Carlos Souza <carlos.pessoal@hotmail.com>
2020-10-09 16:10:25 -0300

    Primeiro requisito

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,9 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  const { animals } = data;
+  return ids.map((id) => animals.find((item) => item.id === id));
 }
 
 function animalsOlderThan(animal, age) {
