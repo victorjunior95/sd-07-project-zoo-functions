93f112c43a79fa2ae13b0c8ba9cbebf85cbcffeb
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-10-09 15:27:16 -0300

    requisito 2 | função animalsOlderThan

--- src/zoo.js
+++ src/zoo.js
@@ -21,7 +21,8 @@ function animalsByIds(...ids) {
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  return animals.find(species => species.name === animal)
+  .residents.every(resident => resident.age > age);
 }
 
 function employeeByName(employeeName) {
