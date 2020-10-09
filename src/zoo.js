f905435d2e19dc9d0502ad5f19f0b2b04b9fac66
Lorena Góes <goesloren@gmail.com>
2020-10-09 17:42:41 -0300

    Requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,26 +11,30 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-// Caso receba nenhum parâmetro, necessário retornar um array vazio
-// Ao receber como parâmetro um único id, retorna os animais com este id
-// Ao receber mais de um id, retorna os animais que têm um desses ids
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+  return data.animals.filter(item => ids.includes(item.id));
 }
 
-// Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada
+// Ao passar o nome de uma espécie e uma idade,
+// testa se todos os animais desta espécie possuem a
+// idade mínima especificada
 function animalsOlderThan(animal, age) {
   // seu código aqui
 }
 
 // Sem parâmetros, retorna um objeto vazio
-// Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
-// Quando provido o último nome do funcionário, retorna o objeto do funcionário
+// Quando provido o primeiro nome do funcionário,
+// retorna o objeto do funcionário
+// Quando provido o último nome do funcionário,
+// retorna o objeto do funcionário
 function employeeByName(employeeName) {
   // seu código aqui
 }
 
-// Cria um novo colaborador a partir de objetos contendo 'informações pessoais' e 'gerentes e animais gerenciados'.
+// Cria um novo colaborador a partir de objetos
+// contendo 'informações pessoais' e 'gerentes
+// e animais gerenciados'.
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
 }
@@ -53,41 +57,56 @@ function animalCount(species) {
 
 // Retorna 0 se nenhum argumento for passado
 // Retorna 0 se um objeto vazio for passado
-// Retorna o preço total a ser cobrado dado o número de adultos, crianças e idosos
+// Retorna o preço total a ser cobrado dado o número de adultos,
+// crianças e idosos
 function entryCalculator(entrants) {
   // seu código aqui
 }
 
 // Sem parâmetros, retorna animais categorizados por localização
-// Com a opção 'includeNames: true' especificada, retorna nomes de animais
-// Com a opção 'sorted: true' especificada, retorna nomes de animais ordenados
-// Com a opção 'sex: 'female'' ou 'sex: 'male'' especificada, retorna somente nomes de animais macho/fêmea
-// Com a opção 'sex: 'female'' ou 'sex: 'male'' especificada e a opção 'sort: true' especificada, retorna somente nomes de animais macho/fêmea com os nomes dos animais ordenados
-// Só retorna informações ordenadas e com sexo se a opção 'includeNames: true' for especificada
+// Com a opção 'includeNames: true' especificada,
+// retorna nomes de animais
+// Com a opção 'sorted: true' especificada, retorna nomes
+// de animais ordenados
+// Com a opção 'sex: 'female'' ou 'sex: 'male'' especificada,
+// retorna somente nomes de animais macho/fêmea
+// Com a opção 'sex: 'female'' ou 'sex: 'male'' especificada e
+// a opção 'sort: true' especificada, retorna somente nomes de
+// animais macho/fêmea com os nomes dos animais ordenados
+// Só retorna informações ordenadas e com sexo se a opção
+// 'includeNames: true' for especificada
 function animalMap(options) {
   // seu código aqui
 }
 
 // Sem parâmetros, retorna um cronograma legível para humanos
-// Se um único dia for passado, retorna somente este dia em um formato legível para humanos
+// Se um único dia for passado, retorna somente este dia
+// em um formato legível para humanos
 function schedule(dayName) {
   // seu código aqui
 }
 
-// Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie
+// Passado o id de um funcionário, encontra a primeira espécie
+// de animal gerenciado pelo funcionário, e retorna um array
+// com nome, sexo e idade do animal mais velho dessa espécie
 function oldestFromFirstSpecies(id) {
   // seu código aqui
 }
 
-// Ao passar uma porcentagem, incrementa todos os preços, arrendondados em duas casas decimais
+// Ao passar uma porcentagem, incrementa todos os preços,
+// arrendondados em duas casas decimais
 function increasePrices(percentage) {
   // seu código aqui
 }
 
-// Sem parâmetros, retorna uma lista de funcionários e os animais pelos quais eles são responsáveis
-// Com o id de um funcionário, retorna os animais pelos quais o funcionário é responsável
-// Com o primeiro nome de um funcionário, retorna os animais pelos quais o funcionário é responsável
-// Com o último nome de um funcionário, retorna os animais pelos quais o funcionário é responsável
+// Sem parâmetros, retorna uma lista de funcionários e os
+// animais pelos quais eles são responsáveis
+// Com o id de um funcionário, retorna os animais pelos quais
+// o funcionário é responsável
+// Com o primeiro nome de um funcionário, retorna os animais
+// pelos quais o funcionário é responsável
+// Com o último nome de um funcionário, retorna os animais
+// pelos quais o funcionário é responsável
 function employeeCoverage(idOrName) {
   // seu código aqui
 }
