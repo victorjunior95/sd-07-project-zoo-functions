40485ed9bccc0f8d0660e306660847412a4226d2
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-10-09 16:08:58 -0300

    animalsByIds - Mais de um Id

--- src/zoo.js
+++ src/zoo.js
@@ -12,10 +12,10 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
     let arr = [];
-    let idAnimals = animals.filter(animals => (
-        animals.id === ids));
+    let idAnimals = animals.filter((animals, i) => (
+        animals.id === ids[i]));
     if (idAnimals !== undefined) {
         arr = (idAnimals);
     }
