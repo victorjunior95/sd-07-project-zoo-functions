4f6d4251dfb1965e807e10573c25c3802f95f271
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-10 11:52:52 -0300

    requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -53,8 +53,15 @@ function isManager(id) {
   return true;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  data.employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
