204f1f4122478ff9585561fd31b7b6fa4353c47e
ericvini <ericcobain3@gmail.com>
2020-10-10 12:23:05 -0300

    Requisito 7

--- src/zoo.js
+++ src/zoo.js
@@ -48,15 +48,24 @@ function isManager(id) {
   }
   return false;
 }
-console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+
 }
 
 function animalCount(species) {
-  // seu código aqui
-}
+  const arrayObjectAnimals = {};
 
+  if (species !== undefined) {
+    return animals.filter(animal => (animal.name === species))
+    .map(animal => animal.residents.length)[0];
+  }
+  animals.forEach(({ name, residents }) => {
+    arrayObjectAnimals[name] = residents.length;
+  });
+  return arrayObjectAnimals;
+}
+console.log(animalCount());
 function entryCalculator(entrants) {
   // seu código aqui
 }
