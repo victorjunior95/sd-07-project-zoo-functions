c7bc4674b5a04bfe681261dda2912462225fbd2f
brendalima <brenda.c.lima@poli.ufrj.br>
2020-10-09 21:08:38 -0300

    Getting started: first commit

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,9 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  const {id} = animals;
+  
 }
 
 function animalsOlderThan(animal, age) {
