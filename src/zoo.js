75a110975ab4b93d7a891645550e493f29fa6d6f
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-10-10 03:46:29 -0300

    3th ready

--- src/zoo.js
+++ src/zoo.js
@@ -24,6 +24,11 @@ function animalsOlderThan(animal, age) {
 
 function employeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees.find(element =>
+    element.firstName === employeeName || element.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
