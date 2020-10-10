02fc9285a661c80d777331dd72baade08fe88240
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-10 11:12:17 -0300

    requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -34,6 +34,16 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  return newEmployee;
 }
 
 function isManager(id) {
