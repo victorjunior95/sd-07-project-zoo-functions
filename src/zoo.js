426cde6159a976c16d63bf91408ad4a9c8bb254f
jvitorof <j.vitoroflores@gmail.com>
2020-10-09 23:06:01 -0300

    :rocket: Req#3 e Req#4 attempt#2 Employee byName and Create / CClimate corrections :rocket:

--- src/zoo.js
+++ src/zoo.js
@@ -27,43 +27,24 @@ function animalsOlderThan(animal, age) {
   return oldBuddy;
 }
 
-function verifyUndefined (arg) {
+function verifyUndefined(arg) {
   if (arg === undefined) return false;
-  else return true;
+  return true;
 }
 
 function employeeByName(employeeName) {
   let zooEmployee = {};
-  let a = employees.find(em => em.firstName === employeeName);
-  let b = employees.find(em => em.lastName === employeeName);
+  const a = employees.find(em => em.firstName === employeeName);
+  const b = employees.find(em => em.lastName === employeeName);
   if (verifyUndefined(a)) zooEmployee = a;
   if (verifyUndefined(b)) zooEmployee = b;
   return zooEmployee;
 }
 
-const personalInfo = {
-  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe',
-};
-
-const associatedWith = {
-  managers: [
-    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992'
-  ],
-  responsibleFor: [
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
-  ]
-};
-
 function createEmployee(personalInfo, associatedWith) {
-  let thisEmployee = {...personalInfo, ...associatedWith,};
+  const thisEmployee = { ...personalInfo, ...associatedWith };
   return thisEmployee;
 }
-console.log(createEmployee(personalInfo, associatedWith))
 
 function isManager(id) {
   // seu código aqui
