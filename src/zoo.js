c00e0dad4021d8177437d599cbf33cd82fd9ca72
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-10-09 21:22:19 -0300

    6)Implemente a função addEmployee.

--- src/zoo.js
+++ src/zoo.js
@@ -54,8 +54,14 @@ function isManager(id) {
   return managersList.some(managerId => managerId === id);
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id = '', firstName = '', lastName = '', managers = [], responsibleFor = []) {
+  return employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
