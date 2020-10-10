102c1af8f1600cd812c09c522810ad95f07df6ee
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-10-10 11:32:30 -0300

    mais funcoes

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals } = require('./data');
+const { animals, employees, prices } = require('./data');
 const data = require('./data');
 
 const animalsByIds = (...ids) => animals.filter(animal => ids.includes(animal.id));
@@ -24,7 +24,7 @@ function employeeByName(employeeName) {
   if (!employeeName) {
     return {};
   }
-  return data.employees
+  return employees
     .find(employee =>
     employee.firstName.includes(employeeName) ||
     employee.lastName.includes(employeeName));
@@ -35,7 +35,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  return data.employees.some(({managers}) => managers.includes(id));
+  return employees.some(({ managers }) => managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
@@ -63,8 +63,16 @@ function oldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  percentage /= 100;
+  const calc = (type, perc) => type * perc;
+  const { Adult, Senior, Child } = prices;
+  prices.Adult = parseFloat(calc(Adult, percentage) + Adult).toFixed(2);
+  prices.Senior = parseFloat(calc(Senior, percentage) + Senior).toFixed(2);
+  prices.Child = parseFloat(calc(Child, percentage) + Senior).toFixed(2);
+  return prices;
 }
+
+console.log(increasePrices(50));
 
 function employeeCoverage(idOrName) {
   // seu código aqui
