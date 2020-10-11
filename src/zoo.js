731b2233eb5d877dcd9da2fb16e63663e85a2b25
Ana Karine <anakarine.ti@gmail.com>
2020-10-11 17:20:13 -0300

    9- Implemente a função animalMap

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,3 @@
-const { animals } = require('./data');
 /*
 eslint no-unused-vars: [
   "error",
@@ -24,9 +23,10 @@ function animalsOlderThan(animal, age) {
 
 function employeeByName(employeeName) {
   if (!employeeName) {
-    return {}
+    return {};
   }
-  return data.employees.find(name => employeeName === name.firstName || employeeName === name.lastName);
+  return data.employees.find(name =>
+    employeeName === name.firstName || employeeName === name.lastName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -45,15 +45,16 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
       firstName,
       lastName,
       managers,
-      responsibleFor
-    }
-  )
+      responsibleFor,
+    },
+  );
 }
 
 function animalCount(species) {
   if (!species) {
     // https://www.digitalocean.com/community/tutorials/js-finally-understand-reduce
-    return data.animals.reduce((acc, curr) => { acc[curr.name] = curr.residents.length;
+    return data.animals.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
       return acc;
     }, {});
     // 1a iteração: acc = {} e curr = animals[0] :: acc = {'lions': 4}
@@ -65,15 +66,56 @@ function animalCount(species) {
 function entryCalculator(entrants) {
   // https://pt.stackoverflow.com/questions/83588/em-javascript-como-verificar-que-um-objeto-est%C3%A1-vazio-sem-jquery
   if (!entrants || Object.keys(entrants).length === 0) {
-    return 0
-  };
+    return 0;
+  }
   const { Adult = 0, Senior = 0, Child = 0 } = entrants;
-  return data.prices.Adult * Adult + data.prices.Senior * Senior + data.prices.Child * Child;
+  return (data.prices.Adult * Adult) + (data.prices.Senior * Senior) + (data.prices.Child * Child);
 }
 
-function animalMap(options) {
-  // seu código aqui
+/* O requisito 9 é do tipo que eu não desenvolveria por conta própria em pouco tempo.
+   Além disso, tive problemas pra interpretar adequadamente as solicitações do mesmo
+   e resolvi analisar projetos de colegas. Abaixo o que julguei o de melhor entendimento.
+   Efetuei mínimas alterações. */
+// https://github.com/tryber/sd-04-block9-project-zoo-functions/pull/26/files
+const includeResidents = (animalName, residents, sorted, sex) => {
+  const obj = {};
+  obj[animalName] = residents.map(resident => resident.name);
+  // {"NE": [{"lions": ["Zena", "Maxwell", "Faustino", "Dee"]}, {"giraffes": ...}
+  if (sex) {
+    obj[animalName] = [];
+    residents.forEach((resident) => {
+      if (resident.sex === sex) {
+        obj[animalName].push(resident.name);
+        // {"NE": [{"lions": ["Zena", "Dee"]}, ...
+      }
+    });
+  }
+  if (sorted) {
+    obj[animalName].sort();
+    // {"NE": [{"lions": ["Dee", "Zena"]}, ...}
+  }
+  return obj;
+};
+
+// https://github.com/tryber/sd-04-block9-project-zoo-functions/pull/26/files
+function animalMap({ includeNames, sorted, sex } = {}) {
+  const result = data.animals.reduce((acc, curr) => {
+    if (!acc[curr.location]) {
+      acc[curr.location] = [];
+      // {"NE": [], "NW": [], "SE": [], "SW": []}
+    }
+    if (!includeNames) {
+      acc[curr.location].push(curr.name);
+      // {"NE": ["lions", "giraffes"], "NW": ["tigers", "bears", "elephants"],
+      // "SE": ["penguins", "otters"], "SW": ["frogs", "snakes"]}
+    } else { // includeNames = true
+      acc[curr.location].push(includeResidents(curr.name, curr.residents, sorted, sex));
+    }
+    return acc;
+  }, {});
+  return result;
 }
+// ---------------------------------------------------------------------------
 
 function schedule(dayName) {
   // seu código aqui
