41d4d8b4bf195af859269a765978b3b091fcdb01
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-10-11 17:49:53 -0300

    Commit 0.2.8

--- src/zoo.js
+++ src/zoo.js
@@ -30,9 +30,29 @@ function employeeByName(...employeeName) {
   return {};
 }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+function createEmployee({ id, firstName, lastName }, { managers, responsibleFor }) {
+  return { id, firstName, lastName, managers, responsibleFor }
 }
+
+const personalInfo = {
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+const associatedWith = {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+};
+
+createEmployee(personalInfo, associatedWith);
 
 function isManager(id) {
   return data.employees.some(({ managers }) => managers.includes(id));
@@ -82,7 +102,7 @@ function oldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+
 }
 
 function employeeCoverage(idOrName) {
