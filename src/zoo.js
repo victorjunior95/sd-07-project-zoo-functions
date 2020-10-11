3ee15d99dc4c226d90a399a07ca0bce14f4444af
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-10-11 13:30:20 -0300

    13)mplemente a função employeeCoverage.

--- src/zoo.js
+++ src/zoo.js
@@ -133,7 +133,24 @@ function increasePrices(percentage) {
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
+  const { employees, animals } = data;
+  const employeesArr = [...employees];
+  const animalsArr = [...animals];
+  const idToName = employee => employee.map(id => animalsArr.find(animal => animal.id === id).name);
+  const allEmployesInfo = {};
+  employeesArr.forEach((employee) => {
+    const { firstName, lastName } = employee;
+    allEmployesInfo[`${firstName} ${lastName}`] = idToName(employee.responsibleFor);
+  });
+  if (idOrName === undefined) return allEmployesInfo;
+  const employeeMatch = employeesArr.filter((employee) => {
+    const { id, firstName, lastName } = employee;
+    return (id === idOrName || firstName === idOrName || lastName === idOrName);
+  });
+  const result = {};
+  const { firstName, lastName } = employeeMatch[0];
+  result[`${firstName} ${lastName}`] = idToName(employeeMatch[0].responsibleFor);
+  return result;
 }
 
 module.exports = {
