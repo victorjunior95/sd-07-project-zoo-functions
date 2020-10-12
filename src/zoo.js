be3ec331665f5519fe4a36bab9a1a5b6eb985c26
LAURO DE SA <itrafor@gmail.com>
2020-10-12 19:18:06 -0300

    Desafio4_1210b

--- src/zoo.js
+++ src/zoo.js
@@ -49,13 +49,10 @@ function employeeByName(employeeName) {
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
   if (personalInfo && associatedWith) {
-    data.employees.push({
-      ...personalInfo,
-      ...associatedWith
-    });
+    data.employees.push({...personalInfo,...associatedWith});
   }
-  let lastEmployee = data.employees.length - 1;
-  let newEmployee = data.employees[lastEmployee];
+  const lastEmployee = data.employees.length - 1;
+  const newEmployee = data.employees[lastEmployee];
   return newEmployee;
 }
 
