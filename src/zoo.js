2ee62da0ce8bae9c9f8a07647af13fe35a77e272
Francisco <franciscoliratorres@gmail.com>
2020-10-12 12:05:13 -0300

    add exercício 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,12 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
-}
+function animalsByIds(...ids) { //usamos o spread para se digitarmos mais de uma id ele aceitar
+  if (typeof ids === undefined) { //primeiro passo é fazer o if falando da condição do undefined
+    return [];
+  };
+  return data.animals.filter((animal) => ids.includes(animal.id)); //em seguida usamos o filter no array animals com os parâmetros currentvalue e index 
+}; //estamos dizendo que o objeto animal no objeto id será igual ao ids na posição index
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
