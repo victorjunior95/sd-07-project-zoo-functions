c3034fd575fb1913f319cac5c658a2febf6a553f
Eduardo Souza <dado.eletrons@gmail.com>
2020-10-12 20:50:12 -0300

    :yum:Realizado createEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -34,6 +34,10 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return {
+    ...personalInfo,
+    ...associatedWith,
+  };
 }
 
 function isManager(id) {
