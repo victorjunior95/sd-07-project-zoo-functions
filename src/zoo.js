5030836d327fb0542f8303b45f45b629b55483cc
Marcos R. Silva <marcos.1998226@gmail.com>
2020-10-12 10:11:52 -0300

    iniciando o projeto

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(ids) {
-  // seu código aqui
+  //iniciando os trabalhos
 }
 
 function animalsOlderThan(animal, age) {
