14b225f3aac757d9bc75ffe57eb9db68c27bd3dd
Eduardo Souza <dado.eletrons@gmail.com>
2020-10-12 22:06:04 -0300

    :grin:Realizado animalCount

--- src/zoo.js
+++ src/zoo.js
@@ -51,7 +51,14 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function animalCount(species) {
-  // seu código aqui
+  if (species === undefined) {
+    return data.animals.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+  }
+
+  return data.animals.find(({ name }) => name === species).residents.length;
 }
 
 function entryCalculator(entrants) {
