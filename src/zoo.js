da1e8ac1870a2640bdcd604a6b655970f2aaf511
samantabelow <samanta.below@gmail.com>
2020-10-13 17:42:50 -0300

    Requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,8 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  return data.animals.filter(animal => ids.includes(animal.id));
 }
 
 function animalsOlderThan(animal, age) {
