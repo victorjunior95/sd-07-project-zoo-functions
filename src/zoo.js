74af727f53bc72db698f9391c7b55255186d5add
André Ferreira <andrefpires1@gmail.com>
2020-10-13 18:50:43 -0300

    teste2

--- src/zoo.js
+++ src/zoo.js
@@ -28,7 +28,7 @@ const employeeByName = (employeeName) => {
     employeeObj = data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
   }
   return employeeObj;
-  //teste
+  //teste2
 };
 
 function createEmployee(personalInfo, associatedWith) {
