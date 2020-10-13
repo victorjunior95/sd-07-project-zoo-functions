b80710255c54203430a00d9aadcff22916d4ac5d
Mauro Junqueira <maurojunqueira@pop-os.localdomain>
2020-10-13 10:22:33 -0300

    Requisito entryCalculator ok

--- src/zoo.js
+++ src/zoo.js
@@ -81,7 +81,14 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (entrants === undefined || entrants === [])
+  return 0;
+  let total = 0;
+  const {Adult, Child, Senior} = entrants;
+  if (Adult !== undefined) total += (Adult * 49.99);
+  if (Child !== undefined) total += (Child * 20.99);
+  if (Senior !== undefined) total += (Senior * 24.99);
+  return total;
 }
 
 function animalMap(options) {
