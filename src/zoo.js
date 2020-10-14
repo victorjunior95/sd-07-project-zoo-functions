5089d44545cc42a28e2084de4de9bf74cd3be482
LAURO DE SA <itrafor@gmail.com>
2020-10-14 04:50:55 -0300

    Desafio 10_1410a

--- src/zoo.js
+++ src/zoo.js
@@ -109,6 +109,21 @@ function animalMap(options) {
 
 function schedule(dayName) {
   // seu código aqui
+  const daysSchedule = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+  if (dayName) {
+    const day = {};
+    day[dayName] = daysSchedule[`${dayName}`];
+    return day;
+  }
+  return daysSchedule;
 }
 
 function oldestFromFirstSpecies(id) {
