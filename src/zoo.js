d09e39330962f437621bb262154489ad1bf3d6e4
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-10-14 13:44:23 -0300

    issue req 7

--- src/zoo.js
+++ src/zoo.js
@@ -78,54 +78,20 @@ function entryCalculator(...entrants) {
   if (Object.keys(entrants).length === 0) {
     return 0;
   }
-  const {Adult, Child, Senior} = data.prices;
+  const { Adult, Child, Senior } = data.prices;
   let totalValue = 0;
-  return entrants.map(value => {
-    if(value.Adult) {
+  return entrants.map((value) => {
+    if (value.Adult) {
       totalValue += value.Adult * Adult;
     }
-    if(value.Child) {
+    if (value.Child) {
       totalValue += value.Child * Child;
     }
-    if(value.Senior) {
+    if (value.Senior) {
       totalValue += value.Senior * Senior;
     }
     return totalValue;
   });
-  
-  /* const adult = Object.keys(entrants).find(item => item === 'Adult');
-  const child = Object.keys(entrants).find(item => item === 'Child');
-  const senior = Object.keys(entrants).find(item => item === 'Senior');
-  
-  if (adult === true && child === true && senior === true) {
-    const {Adult, Child, Senior} = entrants;
-    return Adult * 49.99 + Child * 20.99 + Senior * 24.99;
-  }
-  
-  if (child === true && senior === true) {
-    const {Child, Senior} = entrants;
-    return Child * 20.99 + Senior * 24.99;
-  }
-  if (child === true && adult === true) {
-    const {Child, Adult} = entrants;
-    return Adult * 49.99 + Child * 20.99;
-  }
-  if (senior === true && adult === true) {
-    const {Senior, Adult} = entrants;
-    return Adult * 49.99 + Senior * 24.99;
-  }
-  if (senior === true) {
-    const {Senior} = entrants;
-    return Senior * 24.99;
-  }
-  if (adult === true) {
-    const {Adult} = entrants;
-    return Adult * 49.99;
-  }
-  if (child === true) {
-    const {Child} = entrants;
-    return Child * 20.99;
-  }   */
 }
 
 function animalMap(options) {
