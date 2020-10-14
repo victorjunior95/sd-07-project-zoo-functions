a6fc06fec33e133b32c0f00dc5cab8d2e3ed1444
Anderson Santana <anderslash@gmail.com>
2020-10-14 15:15:13 -0300

    Requisito 05 concluido

--- src/zoo.js
+++ src/zoo.js
@@ -34,7 +34,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(employee => employee.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
