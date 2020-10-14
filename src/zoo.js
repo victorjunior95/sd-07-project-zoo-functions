3c1e339bc22944797bb46de5830532753882c9c2
Anderson Santana <anderslash@gmail.com>
2020-10-14 18:40:08 -0300

    Requisito 10 concluido

--- src/zoo.js
+++ src/zoo.js
@@ -59,7 +59,22 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const { Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday } = data.hours;
+  const completeSchedule = {
+    Tuesday: `Open from ${Tuesday.open}am until ${Tuesday.close - 12}pm`,
+    Wednesday: `Open from ${Wednesday.open}am until ${Wednesday.close - 12}pm`,
+    Thursday: `Open from ${Thursday.open}am until ${Thursday.close - 12}pm`,
+    Friday: `Open from ${Friday.open}am until ${Friday.close - 12}pm`,
+    Saturday: `Open from ${Saturday.open}am until ${Saturday.close - 12}pm`,
+    Sunday: `Open from ${Sunday.open}am until ${Sunday.close - 12}pm`,
+    Monday: 'CLOSED',
+  };
+  if (!dayName) {
+    return completeSchedule;
+  } else if (dayName === 'Monday') return { Monday: 'CLOSED' };
+  return (
+    { [dayName]: `Open from ${data.hours[dayName].open}am until ${data.hours[dayName].close - 12}pm` }
+  );
 }
 
 function oldestFromFirstSpecies(id) {
