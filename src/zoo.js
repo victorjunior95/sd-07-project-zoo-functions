bbbfa0f18ec88c31b8f1e583c34f91ff889b26bc
Cainan Coutinho <cainan6697@gmail.com>
2020-10-14 23:58:39 -0300

    Resolvendo issues do sexto requisito

--- src/zoo.js
+++ src/zoo.js
@@ -37,7 +37,7 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  return employees.push({id, firstName, lastName, managers, responsibleFor,});
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
