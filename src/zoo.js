cb60565687b587db3b5fb96af057fe003a89edac
Luise Rios <luisemiranda@gmail.com>
2020-10-15 10:03:32 -0300

    requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -21,8 +21,12 @@ function animalsOlderThan(animal, age) {
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {}
+  }
+  return data.employees.find(employee => employee.firstName === employeeName || employee.lastName === employeeName);
 }
+
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
