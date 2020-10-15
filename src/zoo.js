ec61384d8f84f406b2d5ecb494e2e99fb4d4c0f7
vini-lima <vini20.henrique@gmail.com>
2020-10-15 01:13:59 -0300

    Function animalMap, schedule, oldestFromFirstSpecies, increasePrices, employeeCoverage

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals, employees, prices } = require('./data');
+const { animals, employees, prices, hours } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -71,24 +71,113 @@ function entryCalculator(entrants) {
   return price.toPrecision(5);
 }
 
+const defaultObject = (arrayLocation, objectAnimalsName) => {
+  arrayLocation.forEach((currentLocation) => {
+    const animalsLocation = animals.filter(cAn => cAn.location === currentLocation);
+    const residentsNames = animalsLocation.map(currentLocal => currentLocal.name);
+    objectAnimalsName[currentLocation] = residentsNames;
+  });
+  return objectAnimalsName;
+};
+
+const calcResidents = (local, options) => {
+  const object = {};
+  const animalsInitialsSex = local.residents
+    .filter(curSex => curSex.sex === options.sex).map(curRes => curRes.name);
+  const animalsInitials = local.residents.map(currRes => currRes.name);
+  if (options.sorted === true && typeof options.sex === 'string') {
+    object[local.name] = animalsInitialsSex.sort();
+    return object;
+  } else if (options.sorted === true && typeof options.sex === 'undefined') {
+    object[local.name] = animalsInitials.sort();
+    return object;
+  } else if (options.sorted === undefined && typeof options.sex === 'string') {
+    object[local.name] = animalsInitialsSex;
+    return object;
+  }
+  object[local.name] = animalsInitials;
+  return object;
+};
+
 function animalMap(options) {
-  // seu código aqui
+  const arrayLocation = ['NE', 'NW', 'SE', 'SW'];
+  const objectAnimalsName = {};
+  if (typeof options === 'undefined' || options.includeNames !== true) {
+    return defaultObject(arrayLocation, objectAnimalsName);
+  }
+  if (options.includeNames === true) {
+    arrayLocation.forEach((currentLocation) => {
+      const animalsLocation = animals.filter(cAni => cAni.location === currentLocation);
+      const residentsNames = animalsLocation.map(local => calcResidents(local, options));
+      objectAnimalsName[currentLocation] = residentsNames;
+    });
+  }
+  return objectAnimalsName;
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const arrayKeys = Object.keys(hours);
+  const scheduleObject = {};
+  const scheduleDayName = {};
+  arrayKeys.forEach((element) => {
+    if (element !== 'Monday') {
+      scheduleObject[element] = `Open from ${hours[element].open}am until ${hours[element].close - 12}pm`;
+    } else {
+      scheduleObject[element] = 'CLOSED';
+    }
+  });
+  if (typeof dayName === 'undefined') {
+    return scheduleObject;
+  }
+  scheduleDayName[dayName] = scheduleObject[dayName];
+  return scheduleDayName;
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+  const arrayResident = [];
+  const animalID = employees.find(currentEmployee => currentEmployee.id === id)
+  .responsibleFor[0];
+  const resident = animals.filter(currentAnimal => currentAnimal.id === animalID)
+  .map(animal => animal.residents.reduce((acc, currentName) => {
+    if (acc.age >= currentName.age) {
+      return acc;
+    }
+    acc = currentName;
+    return acc;
+  }), {});
+  arrayResident.push(resident[0].name, resident[0].sex, resident[0].age);
+  return arrayResident;
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  Object.keys(prices).forEach((keys) => {
+    prices[keys] = Math.round((prices[keys] * (1 + (percentage / 100))) * 100) / 100;
+  });
+  return prices;
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
+  const object = {};
+  employees.forEach((employee) => {
+    const arrayAnimals = [];
+    employee.responsibleFor.forEach((idEmployed) => {
+      animals.forEach((currentAnimal) => {
+        if (currentAnimal.id === idEmployed) {
+          arrayAnimals.push(currentAnimal.name);
+          object[`${employee.firstName} ${employee.lastName}`] = arrayAnimals;
+        }
+      });
+    });
+  });
+  if (typeof idOrName === 'undefined') {
+    return object;
+  }
+  const employeeSearch = employees.filter(employee =>
+    employee.firstName === idOrName || employee.lastName === idOrName || employee.id === idOrName)
+    .map(element => `${element.firstName} ${element.lastName}`);
+  const result = {};
+  result[employeeSearch[0]] = object[employeeSearch];
+  return result;
 }
 
 module.exports = {
