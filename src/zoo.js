d5b62883eb43c029edbc22eb5e66955f0e4a7936
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-10-15 01:17:20 -0300

    Implementação da função create employee

--- src/zoo.js
+++ src/zoo.js
@@ -30,7 +30,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
