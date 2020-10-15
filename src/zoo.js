35aa802871c09c8714684ea54513eeba1bacacea
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-10-15 10:28:05 -0300

    corrigindo issues parte infinito

--- src/zoo.js
+++ src/zoo.js
@@ -101,13 +101,13 @@ function animalMap(options) {
 function schedule(dayName) {
   // seu código aqui
   const newObj = {
-    'Tuesday': 'Open from 8am until 6pm',
-    'Wednesday': 'Open from 8am until 6pm',
-    'Thursday': 'Open from 10am until 8pm',
-    'Friday': 'Open from 10am until 8pm',
-    'Saturday': 'Open from 8am until 10pm',
-    'Sunday': 'Open from 8am until 8pm',
-    'Monday': 'CLOSED'
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED'
   };
   const newObj2 = {};
   if (typeof dayName === 'undefined') {
