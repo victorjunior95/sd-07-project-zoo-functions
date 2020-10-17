4c6685c2ca45dfd82e003a6bf2305ca58aef343b
dennerpl <denner.souzapl@gmail.com>
2020-10-17 12:05:39 -0300

    enviando para verificar o code climate

--- src/zoo.js
+++ src/zoo.js
@@ -58,19 +58,50 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(element => element.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = []
+) {
+  const newEmployee = {
+    id: id,
+    firstName: firstName,
+    lastName: lastName,
+    managers: managers,
+    responsibleFor: responsibleFor,
+  };
+  const result = data.employees;
+  result.push(newEmployee);
+  return result;
 }
 
 function animalCount(species) {
-  // seu código aqui
+  if (species === undefined) {
+    const result = {};
+    data.animals.forEach(
+      animal => (result[animal.name] = animal.residents.length)
+    );
+    return result;
+  }
+  return data.animals.find(element => element.name === species).residents
+    .length;
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  }
+  const { Adult = 0, Child = 0, Senior = 0 } = entrants;
+  return (
+    Adult * data.prices.Adult +
+    Child * data.prices.Child +
+    Senior * data.prices.Senior
+  );
 }
 
 function animalMap(options) {
@@ -78,21 +109,90 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const result = {};
+  const hours = data.hours;
+  if (dayName === undefined) {
+    for (const key in hours) {
+      if (hours.hasOwnProperty(key)) {
+        const element = hours[key];
+        let { open, close } = element;
+        result[key] = `Open from ${open}am until ${close - 12}pm`;
+        if (key === 'Monday') {
+          result[key] = 'CLOSED';
+        }
+      }
+    }
+    return result;
+  }
+  if (dayName === 'Monday') {
+    result[dayName] = 'CLOSED';
+    return result;
+  }
+  let { open, close } = hours[dayName];
+  result[dayName] = `Open from ${open}am until ${close - 12}pm`;
+  return result;
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeResponsableFor = data.employees.find(
+    element => element.id === id
+  ).responsibleFor[0];
+  const older = data.animals
+    .find(element => element.id === employeeResponsableFor)
+    .residents.sort((a, b) => b.age - a.age);
+  return [older[0].name, older[0].sex, older[0].age];
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const copy = data.prices;
+  copy.Adult = copy.Adult * 100;
+  copy.Child = copy.Child * 100;
+  copy.Senior = copy.Senior * 100;
+  copy.Adult = Math.round(
+    (copy.Adult + (copy.Adult * percentage) / 100).toFixed(2)
+  );
+  copy.Child = Math.round(
+    (copy.Child + (copy.Child * percentage) / 100).toFixed(2)
+  );
+  copy.Senior = Math.round(
+    (copy.Senior + (copy.Senior * percentage) / 100).toFixed(2)
+  );
+  copy.Adult = (copy.Adult / 100).toFixed(2);
+  copy.Child = (copy.Child / 100).toFixed(2);
+  copy.Senior = (copy.Senior / 100).toFixed(2);
+  return copy;
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
+  const result = {};
+  if (idOrName === undefined) {
+    data.employees.forEach(element => {
+      let completeName = `${element.firstName} ${element.lastName}`;
+      result[completeName] = [];
+      element.responsibleFor.forEach(idAnimal => {
+        result[completeName].push(
+          data.animals.find(animal => idAnimal === animal.id).name
+        );
+      });
+    });
+    return result;
+  }
+  const employee = data.employees.find(
+    person =>
+      idOrName === person.id ||
+      idOrName === person.firstName ||
+      idOrName === person.lastName
+  );
+  const employeeName = `${employee.firstName} ${employee.lastName}`;
+  result[employeeName] = [];
+  employee.responsibleFor.forEach(idAnimal => {
+    result[employeeName].push(
+      data.animals.find(animal => idAnimal === animal.id).name
+    );
+  });
+  return result;
 }
-
+console.log(employeeCoverage());
 module.exports = {
   entryCalculator,
   schedule,
