469ce9692a60912a167d15f0965708a757fb01d9
brendalima <brenda.c.lima@poli.ufrj.br>
2020-10-17 00:16:04 -0300

    commenting testing lines

--- src/zoo.js
+++ src/zoo.js
@@ -41,8 +41,8 @@ function isManager(id) {
  return checkIfManager;
 }
 
-const test = isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83');
-console.log(test);
+// const test = isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83');
+// console.log(test);
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
