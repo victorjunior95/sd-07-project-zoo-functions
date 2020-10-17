0a13d17dd187b0b03b5153f5cab0449a38a37485
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-10-17 15:41:04 -0300

    Envio atividade 5 para avaliação

--- src/zoo.js
+++ src/zoo.js
@@ -42,7 +42,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some(employee => employee.managers.some(manager => manager === id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
