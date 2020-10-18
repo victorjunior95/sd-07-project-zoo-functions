b0b3344e66ccab4ec3c20a99ce66857660defc19
Camila Valentim <camilaifpb@hotmail.com>
2020-10-18 19:26:00 -0300

    code climate

--- src/zoo.js
+++ src/zoo.js
@@ -67,7 +67,7 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  const { animals, employees, hours, prices } = data;
+  const {hours} = data;
   if (dayName === undefined) {
     const arrHours = Object.entries(hours);
     arrHours.forEach(modifier);
