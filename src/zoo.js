b2d6fce6a92af092b985435933c5027cc2167360
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-10-18 19:38:55 -0300

    Terceiro requisito

--- src/zoo.js
+++ src/zoo.js
@@ -22,7 +22,10 @@ function animalsOlderThan(animal, age) {
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return '';
+  }
+  return data.employees.find(nm => name.firstName === employeeName || nm.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
