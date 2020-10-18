2818e181759104b03e9512c98895386c69e58926
lisvalver <lisvalver.atte@gmail.com>
2020-10-18 13:57:18 -0300

    requesito 3 - employeeByName

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals } = require('./data');
+const { animals, employees } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -23,7 +23,11 @@ function animalsOlderThan(animal, age) {
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find(employee => employee.firstName === employeeName ||
+    employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
