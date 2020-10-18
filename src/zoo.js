8ab7a97e964108beb350de616177cd3d05fe044a
lisvalver <lisvalver.atte@gmail.com>
2020-10-18 14:05:48 -0300

    requesito 4 - createEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -31,7 +31,9 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return { id, firstName, lastName, managers, responsibleFor };
 }
 
 function isManager(id) {
