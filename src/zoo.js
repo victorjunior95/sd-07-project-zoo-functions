b1c17bacd1a91cc03d8176002c7d6b04542bd81c
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-10-18 12:09:03 -0300

    refatoração da função employeeCoverage excluindo conversão desnecessária.

--- src/zoo.js
+++ src/zoo.js
@@ -134,16 +134,14 @@ function increasePrices(percentage) {
 
 function employeeCoverage(idOrName) {
   const { employees, animals } = data;
-  const employeesArr = [...employees];
-  const animalsArr = [...animals];
-  const idToName = employee => employee.map(id => animalsArr.find(animal => animal.id === id).name);
+  const idToName = respFor => respFor.map(id => animals.find(animal => animal.id === id).name);
   const allEmployesInfo = {};
-  employeesArr.forEach((employee) => {
+  employees.forEach((employee) => {
     const { firstName, lastName } = employee;
     allEmployesInfo[`${firstName} ${lastName}`] = idToName(employee.responsibleFor);
   });
   if (idOrName === undefined) return allEmployesInfo;
-  const employeeMatch = employeesArr.filter((employee) => {
+  const employeeMatch = employees.filter((employee) => {
     const { id, firstName, lastName } = employee;
     return (id === idOrName || firstName === idOrName || lastName === idOrName);
   });
