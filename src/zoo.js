ddf0e271bd050cd202b57918bc0493cbbaf1d9f2
Filipi Firmino <contatoic3web@gmail.com>
2020-10-18 16:41:09 -0300

    Adjust CC

--- src/zoo.js
+++ src/zoo.js
@@ -14,22 +14,25 @@ const data = require('./data');
 
 function animalsByIds(ids, ...rest) {
   // seu código aqui
-  const selectedAnimals =
-  animals.filter((animal = []) => animal.id === ids || animal.id === rest[0]);
+  const selectedAnimals = animals.filter(
+    (animal = []) => animal.id === ids || animal.id === rest[0]
+  );
   return selectedAnimals;
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
-  const selected = animals.filter(species => species.name === animal);
-  const result = selected[0].residents.every(iterator => iterator.age >= age);
+  const selected = animals.filter((species) => species.name === animal);
+  const result = selected[0].residents.every((iterator) => iterator.age >= age);
   return result;
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
-  let result = employees.find(employ => employ.firstName === employeeName
-  || employ.lastName === employeeName);
+  let result = employees.find(
+    (employ) =>
+      employ.firstName === employeeName || employ.lastName === employeeName
+  );
   if (result === undefined) {
     result = {};
   }
@@ -45,12 +48,19 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
-  const result = data.employees.map(iterator =>
-  iterator.managers.some(element => element === id));
+  const result = data.employees.map((iterator) =>
+    iterator.managers.some((element) => element === id)
+  );
   return result.includes(true);
 }
 
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = []
+) {
   // seu código aqui
   data.employees.push({
     id,
@@ -61,7 +71,7 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   });
 }
 
-function animalCount(species = ' ') {
+function animalCount(species = '') {
   // seu código aqui
   if (species === ' ') {
     const result = {};
@@ -71,7 +81,7 @@ function animalCount(species = ' ') {
     });
     return result;
   }
-  const dataAnimal = animals.find(animal => animal.name === species);
+  const dataAnimal = animals.find((animal) => animal.name === species);
   return dataAnimal.residents.length;
 }
 
@@ -85,111 +95,131 @@ function entryCalculator(...entrants) {
     return 0;
   }
   let result = 0;
-  entrants.forEach(entries => Object.keys(entries).forEach(item =>
-  (result += prices[item] * entries[item])));
+  entrants.forEach((entries) =>
+    Object.keys(entries).forEach(
+      (item) => (result += prices[item] * entries[item])
+    )
+  );
   return result;
 }
 
 function animalMap(options) {
   // seu código aqui
-  const getDirections = () => {const directions = animals.map((specie) => specie.location)
-  .filter((current, index, self) => index === self.indexOf(current));
-  return directions;
-};
-const sizeInput = Object.keys(options).length;
+  const getDirections = () => {
+    const directions = animals
+      .map((specie) => specie.location)
+      .filter((current, index, self) => index === self.indexOf(current));
+    return directions;
+  };
+  const sizeInput = Object.keys(options).length;
 
   if (options === undefined) {
     let allAnimals = {};
     getDirections().forEach((direction) => {
       let inputItem = [];
-       animals.forEach((itens) => {
-         if(itens.location === direction) {
-           inputItem.push(itens.name)
-           allAnimals[direction] = inputItem;
-        }});
+      animals.forEach((itens) => {
+        if (itens.location === direction) {
+          inputItem.push(itens.name);
+          allAnimals[direction] = inputItem;
+        }
+      });
     });
     return allAnimals;
-  };
+  }
   if (options.includeNames === true && sizeInput === 1) {
     let allAnimals = {};
     getDirections().forEach((direction, indexD) => {
       let inputItem = [];
       let animalName;
-       animals.forEach((itens) => {
-         if(itens.location === direction) {
-          const listAnimal = {}
-           animalName = itens.residents.map(animal => animal.name);
+      animals.forEach((itens) => {
+        if (itens.location === direction) {
+          const listAnimal = {};
+          animalName = itens.residents.map((animal) => animal.name);
           listAnimal[itens.name] = animalName;
-          inputItem.push(listAnimal)
-           allAnimals[direction] = inputItem;
-        }});
-        //Falta corrigir a amostragem do array
+          inputItem.push(listAnimal);
+          allAnimals[direction] = inputItem;
+        }
+      });
+      //Falta corrigir a amostragem do array
     });
     return allAnimals;
-  };
-  if (options.includeNames === true && options.sorted === true && sizeInput === 2) {
+  }
+  if (
+    options.includeNames === true &&
+    options.sorted === true &&
+    sizeInput === 2
+  ) {
     let allAnimals = {};
     getDirections().forEach((direction) => {
       let inputItem = [];
       let animalName;
-       animals.forEach((itens) => {
-         if(itens.location === direction) {
-          const listAnimal = {}
-           animalName = itens.residents.map(animal => animal.name);
-           animalName.sort();
-           listAnimal[itens.name] = animalName;
-           inputItem.push(listAnimal)
-           allAnimals[direction] = inputItem;
-        }});
+      animals.forEach((itens) => {
+        if (itens.location === direction) {
+          const listAnimal = {};
+          animalName = itens.residents.map((animal) => animal.name);
+          animalName.sort();
+          listAnimal[itens.name] = animalName;
+          inputItem.push(listAnimal);
+          allAnimals[direction] = inputItem;
+        }
+      });
     });
     return allAnimals;
-  };
+  }
   if (options.sex && options.includeNames && sizeInput === 2) {
     let allAnimals = {};
     getDirections().forEach((direction) => {
       let inputItem = [];
       let animalName;
-       animals.forEach((itens) => {
-         if(itens.location === direction) {
+      animals.forEach((itens) => {
+        if (itens.location === direction) {
           const listAnimal = {};
-           animalName = itens.residents.filter(animal => animal.sex === options.sex);
-           animalName = animalName.map(currentAnimal => currentAnimal.name);
-           listAnimal[itens.name] = animalName;
-           inputItem.push(listAnimal)
-           allAnimals[direction] = inputItem;
-        }});
+          animalName = itens.residents.filter(
+            (animal) => animal.sex === options.sex
+          );
+          animalName = animalName.map((currentAnimal) => currentAnimal.name);
+          listAnimal[itens.name] = animalName;
+          inputItem.push(listAnimal);
+          allAnimals[direction] = inputItem;
+        }
+      });
     });
     return allAnimals;
-  };
+  }
   if (options.sex && options.sorted === true && sizeInput === 3) {
     let allAnimals = {};
     getDirections().forEach((direction) => {
       let inputItem = [];
       let animalName;
-       animals.forEach((itens) => {
-         if(itens.location === direction) {
+      animals.forEach((itens) => {
+        if (itens.location === direction) {
           const listAnimal = {};
-           animalName = itens.residents.filter(animal => animal.sex === options.sex);
-           animalName = animalName.map(currentAnimal => currentAnimal.name).sort();
-           listAnimal[itens.name] = animalName;
-           inputItem.push(listAnimal);
-           allAnimals[direction] = inputItem;
-        }});
+          animalName = itens.residents.filter(
+            (animal) => animal.sex === options.sex
+          );
+          animalName = animalName
+            .map((currentAnimal) => currentAnimal.name)
+            .sort();
+          listAnimal[itens.name] = animalName;
+          inputItem.push(listAnimal);
+          allAnimals[direction] = inputItem;
+        }
+      });
     });
     return allAnimals;
-  };
+  }
   if (options.includeNames === undefined) {
     let allAnimals = {};
     getDirections().forEach((direction) => {
       let inputItem = [];
-       animals.forEach((itens) => {
-         if(itens.location === direction) {
-           inputItem.push(itens.name)
-           allAnimals[direction] = inputItem;
-        }});
+      animals.forEach((itens) => {
+        if (itens.location === direction) {
+          inputItem.push(itens.name);
+          allAnimals[direction] = inputItem;
+        }
+      });
     });
     return allAnimals;
-    return result;
   }
 }
 function schedule(dayName) {
