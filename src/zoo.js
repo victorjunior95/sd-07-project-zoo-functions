e81f52b73ddf26314ff04ba5300f09c1c45b8f42
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-10-18 17:57:55 -0300

    Alteraçoes para o decimo requisito

--- src/zoo.js
+++ src/zoo.js
@@ -86,7 +86,20 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const { Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday } = data.hours;
+  const theDay = {};
+  const allDays = { Tuesday: `Open from ${Tuesday.open}am until ${Tuesday.close - 12}pm`,
+    Wednesday: `Open from ${Wednesday.open}am until ${Wednesday.close - 12}pm`,
+    Thursday: `Open from ${Thursday.open}am until ${Thursday.close - 12}pm`,
+    Friday: `Open from ${Friday.open}am until ${Friday.close - 12}pm`,
+    Saturday: `Open from ${Saturday.open}am until ${Saturday.close - 12}pm`,
+    Sunday: `Open from ${Sunday.open}am until ${Sunday.close - 12}pm`,
+    Monday: 'CLOSED' };
+  if (dayName) {
+    theDay[dayName] = allDays[dayName];
+    return theDay;
+  }
+  return allDays;
 }
 
 function oldestFromFirstSpecies(id) {
