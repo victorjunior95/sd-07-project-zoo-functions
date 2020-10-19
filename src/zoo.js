8dc7cb237773bbef43732434db301210bcb8bfab
dhiegodias <67385608+dhiegodias@users.noreply.github.com>
2020-10-19 18:23:49 -0300

    Sexto requisito - CC Fix, sou uma anta

--- src/zoo.js
+++ src/zoo.js
@@ -42,7 +42,7 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  return employees.push( {id, firstName, lastName, managers, responsibleFor} );
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
