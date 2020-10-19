a88b9d35f1327b5be09ba037a1ccc441d725b6a4
marcelo ivan <sagacello@hotmail.com>
2020-10-19 17:41:28 -0300

    ultimo?

--- src/zoo.js
+++ src/zoo.js
@@ -14,6 +14,8 @@ const { animals, hours, employees, prices } = require('./data');
 
 
 function animalsByIds(...ids) {
+  // por causa dos ... é necessario o index para capturar o elemento correto
+  // se passar um id ou varios vai dar no mesmo
   if (ids.length === 0) return [];
   return animals.filter((busca, index) => busca.id === ids[index]);
 }
@@ -21,7 +23,11 @@ function animalsByIds(...ids) {
 function animalsOlderThan(animal, age) {
   const nomes = animals.find(busca => busca.name === animal);
   // o metodo find vai me retornar o objeto correspondente a condição
+  // filter vai me retornar cada objeto especifico que satisfez a condiçao
+  // map vai me retornar true ou false para cada ojeto que satisfez
   return nomes.residents.every(idade => idade.age >= age);
+  // de residents para age eu entro em um objeto e portando preciso de um loop para 
+  // acessá lo
   // every vai aplicar para todos os elementos dentro daquele objeto a seguinte condição
   // e se todos os elementos cumprirem aquela condiçao me retorna true se nao false
   /*
@@ -57,12 +63,25 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // find me retorna o objeto todo
-  // com o map eu consigo buscar todos os arrays managers
-  const funcionario = employees.find(busca => busca.id === id);
-  if (funcionario.managers.length > 0) return true;
-  return false;
-}
+  let administradores = employees.find(busca => busca.managers);
+  // tenho como retorno desse map todos os managers em um array dentro de um array
+  // o filter me retorna o objeto inteiro
+  // o find vai me retornar somente o primeiro
+  administradores = administradores.reduce((acc, item) => {
+    item.forEach(elemento => acc.push(elemento))
+    return acc;
+  }, []).includes(id);
+  // O método includes() determina se um array contém um determinado elemento,
+  // retornando true ou false apropriadamente.
+  // administradores vai conter exatamento os managers com um array dentro de outro
+  // depois com o reduce eu vou percorrer o primeiro array e cada item desse array 
+  // representa outro array com os ids , vou percorrer esse segundo array com um foreach
+  // e acrescentar no acumulador cada id formando um array unico com todos os ids
+  // e utilizando o includes eu verifico se o array pssado consta na lista e me retorna
+  // true ou false
+  return administradores;
+  }
+ 
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   employees.push({ id, firstName, lastName, managers, responsibleFor });
@@ -86,6 +105,8 @@ animals.forEach(elemento => {
     }, {});
   }
   const umaEspecie = animals.find(busca => busca.name === species);
+  // o find me retorna o objeto exato 
+  // o filter e o map me retornam dentro de arrays
   return umaEspecie.residents.length;
 }
 
@@ -93,11 +114,23 @@ function entryCalculator(entrants) {
   if (typeof entrants === 'undefined') return 0;
   const { Adult = 0, Child = 0, Senior = 0 } = entrants;
   // desse modo eu preciso passar os mesmos nomes das variaveis internas
+  // essas variaves vao ser numericas começando com o valor 0 inicialmente 
+  // const vai {variaveis } vai receber a entrada e atribuir um novo valor a cada uma
   const precoAdulto = prices.Adult * Adult;
   const precoIdoso = prices.Senior * Senior;
   const precoCrianca = prices.Child * Child;
   const total = precoAdulto + precoIdoso + precoCrianca;
   return total;
+  // FOREAC NAO RETORNA NADA
+  if (entrants === undefined || Object.entries(entrants).length === 0) return 0;
+  return Object.entries(entrants).map(busca => {
+    if (busca[0] === 'Adult') return busca[1] * 49.99
+    if (busca[0] === 'Senior') return busca[1] * 24.99
+    return busca[1] * 20.99
+  }).reduce((acumulador, item) => {
+    acumulador += item
+    return acumulador
+  }, 0)
 }
 
 function animalMap(options) {
@@ -141,7 +174,7 @@ function increasePrices(percentage) {
     return acc[chaves[index]] = (Math.round(item*percentage)/100 + item).toPrecision(4)
   }, {})
   */
-  // o for each acrescenta no objeto diretamente
+  // o for each acrescenta no objeto diretamente , sobrescreve o array original
   const modificaValor = Object.values(prices);
   const chaves = Object.keys(prices);
   modificaValor.forEach((elemento, indice) => {
@@ -151,9 +184,22 @@ function increasePrices(percentage) {
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
+  if (idOrName === undefined ) idOrName = employees;
+  else if (idOrName.split('').length > 20) {
+    idOrName = employees.filter(busca => busca.id === idOrName);
+  }
+  else {
+    idOrName = employees.filter(busca => busca.firstName === idOrName || busca.lastName === idOrName );
+  }
+  return idOrName.reduce((acc, item) => {
+    acc[`${item.firstName} ${item.lastName}`] = item.responsibleFor.map(cadaid => 
+      animals.find(busca => busca.id === cadaid).name);
+      // com o map eu vou conseguir  comparar exatamente cada item dentro de responsibleFor
+      // e para cada comparação eu vou buscar usando o  find um objeto inteiro do animal
+      // que se relaciona com aquele id dentro  de responsibleFor , e então trazer o nome do animal
+    return acc;
+  },{});
 }
-
 module.exports = {
   entryCalculator,
   schedule,
