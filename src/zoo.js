f9e70752c6d71f9cafa58cf6db6d36409e4faa1a
Cainan Coutinho <cainan6697@gmail.com>
2020-10-19 02:30:50 -0300

    Oitavo requisito, entryCalculator

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals, employees } = require('./data');
+const { animals, employees, prices } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -56,7 +56,11 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  return Object.keys(entrants).reduce((total, key) =>
+  total + (prices[key] * entrants[key]), 0);
 }
 
 function animalMap(options) {
