28756d6663605b870076b0b1c351e9427b0dd325
Brune Miguel <bstfreire@gmail.com>
2020-10-19 11:48:23 -0300

    Corrigindo CC

--- src/zoo.js
+++ src/zoo.js
@@ -7,8 +7,7 @@ eslint no-unused-vars: [
     "varsIgnorePattern": "data"
   }
 ]
-*/
- 
+*/ 
 const { employees } = require('./data');
 const data = require('./data');
 
@@ -28,9 +27,9 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
- const { id, firstName, lastName} = personalInfo;
- const { managers, responsibleFor} = associatedWith
- return `${personalInfo} ${associatedWith}`
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return {id, firstName, lastName, managers, responsibleFor};
 }
 
 function isManager(id) {
