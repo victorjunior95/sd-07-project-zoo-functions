c2506f2c65d4056c2d6bc3e9ec008eac83cdd14f
Emerson Nogueira Junqueira <emoonlit@pop-os.localdomain>
2020-10-19 18:30:02 -0300

    Solutions

--- src/zoo.js
+++ src/zoo.js
@@ -9,30 +9,65 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals, employees } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui Iniciando
+function animalsByIds(...ids) {
+  /* Comentários mantidos para fins didáticos.
+  Meu código:
+  const animalsId = [];
+  ids.forEach(id => animalsId.push(data.animals.find(animal => animal.id === id)));
+  return animalsId;*/
+
+  // Solução apresentada pelo instrutor Hamaji:
+  return animals.filter(animal => ids.includes(animal.id));
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  return animals
+    .find(element => element.name === animal)
+    .residents.every(resident => resident.age > age);
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const functionary = employees.find(name =>
+    name.firstName === employeeName || name.lastName === employeeName);
+  return functionary;
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
 }
 
 function isManager(id) {
   // seu código aqui
+  return employees.some(functionary => functionary.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  const functionary = employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
+  return functionary;
 }
 
 function animalCount(species) {
