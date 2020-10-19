a01dba8666b1427d18497839a265ad29fcc75729
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-10-19 16:09:43 -0300

    função animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -10,9 +10,11 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { animals } = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  const idsAnimals = animals.filter(animal => ids.includes(animal.id));
+  return idsAnimals;
 }
 
 function animalsOlderThan(animal, age) {
