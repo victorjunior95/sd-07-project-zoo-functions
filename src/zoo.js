07b0c1fa1f6c3c89fde53da1e866e6459b74af21
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-10-19 19:30:34 -0300

    função animalCount

--- src/zoo.js
+++ src/zoo.js
@@ -64,7 +64,14 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 }
 
 function animalCount(species) {
-  // seu código aqui
+  if (species === undefined) {
+    return animals.reduce((animalObj, animal) => {
+      animalObj[animal.name] = animal.residents.length;
+      return animalObj;
+    }, {});
+  }
+
+  return animals.find(animal => animal.name === species).residents.length;
 }
 
 function entryCalculator(entrants) {
