8560f574e4da2c88bfac12ed995829ca7cb4b88b
Renato Silva <renatosdev@gmail.com>
2020-10-19 09:19:14 -0300

    Requisito 02

--- src/zoo.js
+++ src/zoo.js
@@ -9,15 +9,17 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+const data = require("./data");
 
 function animalsByIds(...ids) {
-  return data.animals.filter(animal => ids.includes(animal.id));
+  return data.animals.filter((animal) => ids.includes(animal.id));
 }
 // ensinado pelo Wolf
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.animals
+    .find((specie) => specie.name === animal)
+    .residents.every((minimumAge) => minimumAge.age > age);
 }
 
 function employeeByName(employeeName) {
