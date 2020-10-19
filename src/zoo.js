5a54b1888411105e2fc621478c4115909214c540
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-10-18 21:14:39 -0300

    requisitos 5 e 6

--- src/zoo.js
+++ src/zoo.js
@@ -43,16 +43,55 @@ function createEmployee(personalInfo, associatedWith) {
   return Object.assign(personalInfo, associatedWith);
 }
 
+// const manager = data.employees.map(employee => employee.managers);
+// console.log(manager);
+// const result = manager.flat(Infinity);
+// console.log(result);
+// const isMng = result.some(id => id === '0e7b460e-acf4-4e17-bcb3-ee472265db83');
+// console.log(isMng);
+// const result = manager.forEach(element => {
+//   return element.some(id => id === '0e7b460e-acf4-4e17-bcb3-ee472265db83')
+// });
+
+
+// let arrayManagers = [];
+// const ids = [...manager];
+// console.log(ids);
+// console.log(manager.some(idManager => idManager === '0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+
+// console.log(idsManagers);
 function isManager(id) {
-  // seu código aqui
+  const managers = data.employees.map(employee => employee.managers);
+  const arrayManagers = managers.flat(Infinity);
+
+  return arrayManagers.some(idMng => idMng === id);
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id1, firstName1, lastName1, managers1, responsibleFor1) {
+  if (managers1 === undefined) managers1 = [];
+  if (responsibleFor1 === undefined) responsibleFor1 = [];
+  const newEmployee = {
+    id: id1,
+    firstName: firstName1,
+    lastName: lastName1,
+    managers: managers1,
+    responsibleFor: responsibleFor1,
+  };
+
+  return data.employees.push(newEmployee);
 }
 
+// const allAnimals = data.animals.map(animal => animal.name);
+// console.log(allAnimals);
+// const numberAnimals = data.animals.map(animal => animal.popularity);
+// console.log(numberAnimals);
+// let objAnimals = {};
+// const object = allAnimals.reduce((animal, currentAnimal, index) => {animal, currentAnimal}, {});
+// console.log(object);
 function animalCount(species) {
-  // seu código aqui
+  // if (species === null) {
+  //   const allAnimals = data.animals.map(animal => animal.name);
+  // }
 }
 
 function entryCalculator(entrants) {
@@ -62,7 +101,15 @@ function entryCalculator(entrants) {
 function animalMap(options) {
   // seu código aqui
 }
-
+// const expected = {
+//   'Tuesday': 'Open from 8am until 6pm',
+//   'Wednesday': 'Open from 8am until 6pm',
+//   'Thursday': 'Open from 10am until 8pm',
+//   'Friday': 'Open from 10am until 8pm',
+//   'Saturday': 'Open from 8am until 10pm',
+//   'Sunday': 'Open from 8am until 8pm',
+//   'Monday': 'CLOSED'
+// };
 function schedule(dayName) {
   // seu código aqui
 }
