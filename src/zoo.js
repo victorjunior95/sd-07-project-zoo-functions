9170f8d9533c2409c94d71a0d266a23f83186719
nyxnil <nsmnilson@hotmail.com>
2020-10-20 18:34:39 -0300

    refatorando codigos

--- src/zoo.js
+++ src/zoo.js
@@ -12,18 +12,14 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  if (ids.length === 0) {
-    return [];
-  }
   return ids.map(idAnimal => data.animals.find(animals => animals.id === idAnimal));
 }
 
 function animalsOlderThan(animal, age) {
-  const animalList = data.animals.find(animalName => animalName === animal);
-  const animalAge = animalList.residents.every(animalName => animalName === age);
+  const animalList = data.animals.find(animalName => animalName.name === animal);
+  const animalAge = animalList.residents.every(animalName => animalName.age >= age);
   return animalAge;
 }
-
 function employeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
@@ -34,8 +30,18 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  const employees = data.employees;
-  return [...employees, personalInfo, associatedWith]
+  const newEmployee = {
+    id: personalInfo.id,
+    firstName: personalInfo.firstName,
+    lastName: personalInfo.lastName,
+    managers: [
+      associatedWith.managers[0],
+    ],
+    responsibleFor: [
+      associatedWith.responsibleFor[0],
+    ],
+  };
+  return newEmployee;
 }
 
 function isManager(id) {
@@ -47,7 +53,6 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
 }
 
 function animalCount(species) {
@@ -58,8 +63,22 @@ function entryCalculator(entrants) {
   // seu código aqui
 }
 
+// const objectInicial = {
+//   NE = [],
+//   NW = [],
+//   SE = [],
+//   Sw = [],
+// }
+
 function animalMap(options) {
-  // seu código aqui
+  // return data.animals.reduce((acc, specie) => {
+  //   return {
+  //     ...acc,[specie.location]: [
+  //       ...acc[specie.location],
+  //       specie.name
+  //     ]
+  //   };
+  // }, objectInicial);
 }
 
 function schedule(dayName) {
