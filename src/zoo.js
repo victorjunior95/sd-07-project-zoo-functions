d3739cc08ab145c11ac96cc7c112d14ebcff1afd
Alstent <rafaelasltent@gmail.com>
2020-10-20 06:17:00 +0200

    implementando a funcao creatManager e isManager

--- src/zoo.js
+++ src/zoo.js
@@ -32,11 +32,11 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some(employee => employee.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
