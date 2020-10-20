8a305655e7ad60358ef8ca2eb321aaefbb1c3f10
renatomak <renato.mark.silva@gmail.com>
2020-10-20 18:21:18 -0300

    requisito 9-CC

--- src/zoo.js
+++ src/zoo.js
@@ -1,14 +1,3 @@
-/*
-eslint no-unused-vars: [
-  "error",
-  {
-    "args": "none",
-    "vars": "local",
-    "varsIgnorePattern": "data"
-  }
-]
-*/
-
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -227,12 +216,10 @@ function animalMap(options) {
     if (sorted) {
       return categorizeAnimalsIncludeNamesSorted();
     }
-
-
     return categorizeAnimalsIncludeNames();
   }
 
-  if (sex){
+  if (sex || !options){
     return categorizeAnimalsByLocation();
   }
 }
