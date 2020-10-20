7c5fc76d37e4d75f9a69ec069e09a1be2e0e8c56
Galaraz <jvictor.si.2013@gmail.com>
2020-10-19 22:34:43 -0300

    entryCalculator

--- src/zoo.js
+++ src/zoo.js
@@ -52,11 +52,26 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 }
 
 function animalCount(species) {
-  // seu código aqui
+  // seu código aqui   
 }
 
 function entryCalculator(entrants) {
   // seu código aqui
+  const paying = {...entrants};
+ 
+  if (paying === undefined || paying === []){
+    return 0;
+  }
+  let total = 0;
+  
+  const {Adult, Child, Senior} = paying;
+  if (Adult !== undefined) {total += (Adult *49.99)}
+  
+  if (Child !== undefined) {total += (Child *20.99)}
+  
+  if (Senior !== undefined) {total += (Senior *24.99)}
+  
+  return total;
 }
 
 function animalMap(options) {
