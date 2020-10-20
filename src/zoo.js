13f0e6e595577b6db8e12c534c434aaaf2c36390
Brune Miguel <bstfreire@gmail.com>
2020-10-20 19:02:31 -0300

    Refatorando requisito 7

--- src/zoo.js
+++ src/zoo.js
@@ -40,15 +40,13 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function animalCount(species) {
-  if (species){
-   return data.animals.find(animal.name === species).popularity;
-    
+  if(!species) {
+    return data.animals.reduce((accAnimal, animal) => {
+      accAnimal[animal.name] = animal.residents.length 
+      return accAnimal
+    }, {})
   }
-   objectAnimal = {};
-   const allAnimals = data.animals.map(animal => animal.name);
-   const allAnimalsAcc = data.animals.map(animal => animal.popularity);
-   allAnimals.forEach((animal, index) =>
-   objectAnimal[animal] = allAnimalsAcc[index]);
+  return data.animals.find(animal => animal.name === species).residents.length
 }
 
 function entryCalculator(entrants) {
