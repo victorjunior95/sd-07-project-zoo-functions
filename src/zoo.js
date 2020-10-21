b62040411944469b8fc0472f60465b4231e958ce
Alstent <rafaelasltent@gmail.com>
2020-10-21 21:24:09 +0200

    implementando a funcao animalMap

--- src/zoo.js
+++ src/zoo.js
@@ -17,8 +17,8 @@ const { employees } = data;
 function animalsByIds(...ids) {
   if (ids === undefined) return [];
   return animals.filter(animal => ids.includes(animal.id));
-  // return id.filter(animalId => animalId.id == ids) !! PK NAO FUNCIONA ASSIM? !!
-  // return id.filter(animalId => animalId.id.includes(ids)) !! PK NAO FUNCIONA ASSIM? !!
+  
+  // tentar com foreach
 }
 
 function animalsOlderThan(animal, age) {
@@ -51,8 +51,65 @@ function entryCalculator(entrants) {
   // seu código aqui
 }
 
+function retrieveAvailableLocations() {
+  return ['NE', 'NW', 'SE', 'SW'];
+}
+
+function retrieveFilteredAnimalsByLocation(location) {
+  return animals.filter((animal) => animal.location === location);
+}
+
+function retrieveAnimalsPerLocation(locations) {
+  const animalPerLocation = {};
+
+  locations.forEach((location) => {
+    const filteredAnimals = retrieveFilteredAnimalsByLocation(location)
+    .map((animal) => animal.name);
+    // const filteredAnimals = animals.filter(animal => location.includes(animal.location)).map((animal) => animal.name); !! OUTRA ALTERNATIVA !!
+
+    if (filteredAnimals.length !== 0) animalPerLocation[location] = filteredAnimals; // DIFERENTE DE --> animalPerLocation.location = filteredAnimals (.location seria o nome pra todas as keys) !! adicionando ao obj com a key ${location} !!
+  });
+
+  return animalPerLocation;
+}
+
+function retrieveAnimalsPerLocationWithName (locations, sorted, sex) {
+  const animalsPerLocation = {};
+
+  locations.forEach((location) => {
+    const filteredAnimals = retrieveFilteredAnimalsByLocation(location).map((animal) => {
+      const animalName = animal.name;
+      const residents = animal.residents.filter((resident) => {
+        const needFilterSex = sex !==  false // sex === 'male' ||  'female' !! alternativa !! ${needFilterSex} GUARDA UMA BOOLEAN DA COMPARACAO FEITA EM sex !== false (se ${sex} for diferente de 'false', ira mandar pra ${needFilterSex} o resultado 'true')!! 
+        // return needFilterSex ? resident.sex === sex : true; !! operador ternario !!
+        if (needFilterSex) { // (se o resultado for true, ele ira entrar no 'if'!)
+           return resident.sex === sex;
+        } else {
+          return true;
+        }
+      }).map(resident => resident.name);
+      
+      if (sorted) residents.sort();
+
+      return { [animalName]: residents };
+    });
+    if (filteredAnimals !== 0) animalsPerLocation[location] = filteredAnimals;
+  });
+  return animalsPerLocation;
+}
+
 function animalMap(options) {
-  // seu código aqui
+  const locations = retrieveAvailableLocations();
+
+  if (!options) return retrieveAnimalsPerLocation(locations); // if (false === false) | if (!options === false) roda o codigo
+
+  const { includeNames = false, sorted = false, sex = false } = options;
+
+  if (includeNames) { // if (true === true) | if (options === true) roda o codigo
+    return retrieveAnimalsPerLocationWithName(locations, sorted, sex);
+  } else {
+    return retrieveAnimalsPerLocation(locations);
+  }
 }
 
 function schedule(dayName) {
