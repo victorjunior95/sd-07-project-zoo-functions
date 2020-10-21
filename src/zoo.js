7e640ae55817002d278a4cb5005998da48cddda3
Brune Miguel <bstfreire@gmail.com>
2020-10-21 11:53:00 -0300

    Corrigindo CC

--- src/zoo.js
+++ src/zoo.js
@@ -8,7 +8,7 @@ eslint no-unused-vars: [
   }
 ]
 */
-const { animals } = require('./data');
+
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -54,75 +54,71 @@ function animalCount(species) {
 function entryCalculator(entrants) {
   if ((!entrants) || (Object.keys(entrants).length === 0)) {
     return 0;
-  } else {
-    const entrantKeys = Object.keys(entrants);
-    const entrantValue = Object.values(entrants);
-    const result = entrantKeys.reduce( (sumAcc, currentValue, index) => {
-      const currentCalc = data.prices[currentValue] * entrantValue[index];
-      return sumAcc + currentCalc;
-    },0);
-    return result;
   }
+  const entrantKeys = Object.keys(entrants);
+  const entrantValue = Object.values(entrants);
+  const result = entrantKeys.reduce((sumAcc, currentValue, index) => {
+    const currentCalc = data.prices[currentValue] * entrantValue[index];
+    return sumAcc + currentCalc;
+  }, 0);
+  return result;
+
 }
 
 function retrieveAvailableLocations() {
   return data.animals
-  .map( (animal) => animal.location )
-  .reduce( (acc, currentValue) => {
+  .map((animal) => animal.location)
+  .reduce(acc, currentValue => {
     const currentValueAlreadyExistsInAcc = currentValue.includes(acc);
     return currentValueAlreadyExistsInAcc ? acc : [...acc, currentValue];
   });
 }
 
 function retrieveFilteredAnimalsByLocation(location) {
-   return data.animals.filter( (animal) => animal.location === location )
+  return data.animals.filter(animal => animal.location === location);
 }
 
 function retrieveAnimalsPerLocation(locations) {
-  //Sem parâmetros, retorna animais categorizados por localização
-  const animalsPerLocation = {};
-  
-  locations.forEach( (location) => {
-    const filteredAnimals = retrieveFilteredAnimalsByLocation(location)
-      .map( (animal) => animal.name);
 
-      if (filteredAnimals.length !== 0) animalsPerLocation[location] = filteredAnimals;
+  const animalsPerLocation = {};
+
+  locations.forEach((location) => {
+    const filteredAnimals = retrieveFilteredAnimalsByLocation(location)
+      .map(animal => animal.name);
+
+    if (filteredAnimals.length !== 0) animalsPerLocation[location] = filteredAnimals;
   });
 
-  return animalsPerLocation; 
+  return animalsPerLocation;
 }
 
-function retrieveAnimalsPerLocationsWithName(locations, sorted, sex){
+function retrieveAnimalsPerLocationsWithName(locations, sorted, sex) {
   const animalsPerLocation = {};
 
-  locations.forEach( (location) => {
+  locations.forEach((location) => {
     const filteredAnimals = retrieveFilteredAnimalsByLocation(location)
-    .map( (animal) => {
+    .map((animal) => {
       const animalName = animal.name;
       const residents = animal.residents
-      .filter( (resident) => {
+      .filter((resident) => {
         const needFiltering = sex !== undefined;
-        //return needFiltering ? resident.sex === sex : true;
+      
         if (needFiltering) {
           return resident.sex === sex;
-        } else {
-          return true;
-        }
+        } return true;
       })
-      .map( (resident) => resident.name );
+      .map(resident => resident.name);
 
       if (sorted) residents.sort();
 
       return { [animalName]: residents };
-    })
+    });
 
     if (filteredAnimals.length !== 0) animalsPerLocation[location] = filteredAnimals;
-      
   })
   return animalsPerLocation;
 }
-
-//Requisito resolvido em aula ao vivo c/ Oliva
+// Requisito resolvido em aula ao vivo c/ Oliva
 function animalMap(options) {
   const locations = retrieveAvailableLocations();
 
@@ -132,10 +128,7 @@ function animalMap(options) {
 
   if (includeNames) {
     return retrieveAnimalsPerLocationsWithName(locations, sorted, sex);
-  } else {
-    return retrieveAnimalsPerLocation(locations);
-  }
-
+  } return retrieveAnimalsPerLocation(locations);
 }
 
 function schedule(dayName) {
