1387fd72e76e0df1e248fb7935b9ea8eb07b0816
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-10-20 22:10:16 -0300

    Requirement 9

--- src/zoo.js
+++ src/zoo.js
@@ -83,8 +83,58 @@ function entryCalculator(entrants) {
   return totalValue;
 }
 
-function animalMap(options) {
+function retrieveAvailableLocations() {
+  return ['NE', 'E', 'NW', 'SW', 'SE'];
 }
+function retrieveFilteredAnimalByLocation(location) {
+  return animals.filter((animal) => animal.location === location);
+}
+function retrieveAnimalsPerLocation(locations) {
+  const animalsPerLocation = {};
+  locations.forEach((location) => { 
+    const filteredAnimals = retrieveFilteredAnimalByLocation(location).map((animal) => animal.name);
+      if (filteredAnimals.length !== 0) animalsPerLocation[location] = filteredAnimals;
+  });
+  return animalsPerLocation;
+}
+
+function retrieveAnimalsPerLocationWithName(locations, sorted, sex) {
+  const animalsPerLocation = {};
+  locations.forEach((location) => {
+    const filteredAnimals = retrieveFilteredAnimalByLocation(location).map((animal) => {
+      const animalName = animal.name
+      const residents = animal.residents
+        .filter((resident) => {
+          const needFiltering = sex !== undefined;
+          // needFiltering ? resident.sex === sex : true; Ternary operator
+          if (needFiltering) {
+            return resident.sex === sex;
+          } else {
+            return true;
+          }
+        })
+        .map((residents) => residents.name);
+      if (sorted) residents.sort();
+      return { [animalName]: residents };
+    });
+    if (filteredAnimals.length !== 0) animalsPerLocation[location] = filteredAnimals;
+  });
+  return animalsPerLocation;
+}
+// Funcao animalMap:
+// Para implementar esta funcao, segui a resolucao do mestre Oliva.
+// options === object
+// Which options 'options' can have?
+// includeNames: bool
+// sorted: bool
+// sex: string
+function animalMap(options) {
+  const locations = retrieveAvailableLocations();
+  if (!options) return retrieveAnimalsPerLocation(locations);
+  const { includeNames = false, sorted = false, sex } = options;
+  if (includeNames) return retrieveAnimalsPerLocationWithName(locations, sorted, sex);
+    return retrieveAnimalsPerLocation(locations);
+} 
 
 // Funcao schedule:
 // Para implementar esta funcao, consultei o repositorio do colega Ygor Fonseca.
