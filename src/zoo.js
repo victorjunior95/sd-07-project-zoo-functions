1c3727147389525d35afe31d5432103443391a41
afonsofn <afonsoafn@gmail.com>
2020-10-20 21:40:38 -0300

    requesito 6

--- src/zoo.js
+++ src/zoo.js
@@ -49,8 +49,14 @@ function isManager(id) {
   return data.employees.some(employee => employee.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
