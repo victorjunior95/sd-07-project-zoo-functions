218ac317cef3a3bd1c69bc5c9955dede479e5b05
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-10-22 12:57:07 -0300

    Atualizando - 22/10/2020

--- src/zoo.js
+++ src/zoo.js
@@ -18,7 +18,6 @@ function animalsByIds(...ids) {
   return animals.filter((animal, index) => animal.id === ids[index]);
 }
 // console.log(animalsByIds())
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
 function animalsOlderThan(animal, age) {
   const myAnimal = animals.find(animalName => animalName.name === animal);
