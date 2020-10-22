072711b17457c2f6e7e916b53a62232c4aa1cd96
barrrosomarcio <barrrosomarcio@gmail.com>
2020-10-22 18:15:05 -0300

    1 e 2

--- test/animalsByIds.test.js
+++ test/animalsByIds.test.js
@@ -25,6 +25,7 @@ describe('Implemente a função animalsByIds', () => {
 
     assert.deepEqual(actual, expected);
   });
+
   it('Ao receber mais de um id, retorna os animais que têm um desses ids', () => {
     const actual = zoo.animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
     const expected = [{
