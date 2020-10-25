39ead41e7578c641a3b797fa834bd5c5ab9420e5
Galaraz <jvictor.si.2013@gmail.com>
2020-10-25 18:41:10 -0300

    Cosmo Elevadogit add .

--- src/zoo.js
+++ src/zoo.js
@@ -1,41 +1,37 @@
 /*
 eslint no-unused-vars: [
-  "error",
+  'error',
   {
-    "args": "none",
-    "vars": "local",
-    "varsIgnorePattern": "data"
+    'args': 'none',
+    'vars': 'local',
+    'varsIgnorePattern': 'data'
   }
 ]
 */
 
-const data = require("./data");
+const data = require('./data');
 
 function animalsByIds(...ids) {
   // Versao Tunada em aula e ingles!!!
-  const compare = data.animals.filter((animal) => ids.includes(animal.id));
+  const compare = data.animals.filter(animal => ids.includes(animal.id));
   return compare;
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
-  const catchAnimals = data.animals.filter(
-    (species) => species.name === animal
-  );
-  const olderThan = catchAnimals[0].residents.every(
-    (older) => older.age >= age
-  );
+  const catchAnimals = data.animals.filter(species => species.name === animal);
+  const olderThan = catchAnimals[0].residents.every(older => older.age >= age);
   return olderThan;
 }
 
 function employeeByName(employeeName) {
   // carregado na raça
   const personName = employeeName;
-  if (typeof personName === "undefined") {
+  if (typeof personName === 'undefined') {
     return {};
   }
   console.log(personName);
-  const getPerson = (person) =>
+  const getPerson = person =>
     personName === person.firstName || personName === person.lastName;
   const findPerson = data.employees.find(getPerson);
   return findPerson;
@@ -49,20 +45,12 @@ function createEmployee(personalInfo, associatedWith) {
 function isManager(id) {
   // try hard my friend
   const compareId = [];
-  data.employees.map((grup) =>
-    grup.managers.forEach((grupElement) => compareId.push(grupElement))
-  );
+  data.employees.map(grup => grup.managers.forEach(grupElement => compareId.push(grupElement)));
   const cathId = compareId.some(grup => grup === id);
   return cathId;
 }
 
-function addEmployee(
-  id,
-  firstName,
-  lastName,
-  managers = [],
-  responsibleFor = []
-) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
   data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
@@ -70,10 +58,10 @@ function addEmployee(
 function animalCount(species) {
   // seu código aqui
   if (species) {
-    const expectedObject = animals.find((item) => item.name === species);
+    const expectedObject = data.animals.find(item => item.name === species);
     return expectedObject.residents.length;
   }
-  return animals.reduce((result, item) => {
+  return data.animals.reduce((result, item) => {
     const currentName = item.name;
     const currentLength = item.residents.length;
     result[currentName] = currentLength;
@@ -87,20 +75,14 @@ function entryCalculator(entrants) {
   if (paying === undefined || paying === []) {
     return 0;
   }
-  return Object.keys(paying).reduce(
+  const result = Object.keys(paying).reduce(
     (sum, currentValue) =>
-      sum + entrants[currentValue] * data.prices[currentValue],
-    0
-  );
+      (sum += paying[currentValue] * data.prices[currentValue])
+      , 0);
+  return result;
 }
 
 function animalMap(options) {
-  const catchPrices = data.prices;
-  for (let i in catchPrices) {
-    const increase = catchPrices[i] * (percentage / 100);
-    const around = Math.round((catchPrices[i] + increase) * 100) / 100;
-    catchPrices[i] = around;
-  }
   // seu código aqui
 }
 
@@ -115,10 +97,10 @@ function oldestFromFirstSpecies(id) {
 function increasePrices(percentage) {
   // carregado pelo moises
   const catchPrices = Object.entries(data.prices);
-
+  console.log(catchPrices);
   catchPrices.forEach((i) => {
     i[1] += (i[1] * percentage) / 100;
-    i[1] = Math.round(i[1]*100) / 100;
+    i[1] = Math.round(i[1] * 100) / 100;
     data.prices[i[0]] = i[1];
   });
 }
