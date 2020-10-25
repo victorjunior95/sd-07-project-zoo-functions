67d6be81569876d1f0095fa5fa3be8028644e628
André Ferreira <andrefpires1@gmail.com>
2020-10-25 18:12:57 -0300

    Função increasePrices (Primeira solução) - CC 33

--- src/zoo.js
+++ src/zoo.js
@@ -117,10 +117,17 @@ const decimalAdjust = (price) => {
 const increasePrices = (percentage) => {
   const entries = Object.entries(data.prices);
   const multiplier = percentage / 100;
-  const newPrices = entries.map(array => array[1] += array[1] * multiplier);
-  let increasePrices = {};
-  newPrices.map((price, i) => increasePrices[entries[i][0]] = decimalAdjust(price));
-  return increasePrices;
+  const newPrices = entries.map(array => {
+    const increasePrice = array[1] += array[1] * multiplier;
+    return increasePrice;
+  });
+  const newObjectPrices = {};
+  newPrices.map((newPrice, i) => {
+    const price = newPrice;
+    return newObjectPrices[entries[i][0]] = decimalAdjust(price);
+  });
+
+  return newObjectPrices;
 };
 
 const objectWithEmployeesNamesAndAnimals = () => {
