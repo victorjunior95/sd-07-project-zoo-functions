7b0d5ec3dfb4a9b91073b7343449936e5a9c1f37
gabrieldeori <deori.pessoal@gmail.com>
2020-10-25 11:14:45 -0300

    oldestFromFirstSpecies

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,3 @@
-const { animals, employees, prices, hours } = require('./data');
 /*
 eslint no-unused-vars: [
   "error",
@@ -11,8 +10,9 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { animals, employees, prices, hours } = data;
 
-function animalsByIds(...ids) {
+function animalsByIds(ids) {
   let response;
   if (ids.length > 1) {
     response = animals.filter((animal, posicao) => animal.id === ids[posicao]);
@@ -79,71 +79,14 @@ function animalCount(species) {
   return response;
 }
 
-function entryCalculator(entrants = {}) {
+function entryCalculator(entrants) {
   let totalPrice = 0;
   Object.keys(entrants).forEach(entrant => (totalPrice += prices[entrant] * entrants[entrant]));
   return totalPrice;
 }
 
-function retrieveAvailableLocations() {
-  return data.animals
-  .map(animal => animal.location)
-  .reduce((acc, currentValue) => {
-    const currentValueAlreadyExistsInAcc = currentValue.includes(acc);
-    return currentValueAlreadyExistsInAcc ? acc : [...acc, currentValue];
-  });
-}
-function retrieveFilteredAnimalsByLocation(location) {
-  return data.animals.filter(animal => animal.location === locations);
-}
-function retrieveAnimalsPerLocation(locations) {
-  // Sem parâmetros, retorna animais categorizados por localização
-  const animalsPerLocation = {};
-  locations.forEach(location => {
-    const filteredAnimals = retrieveFilteredAnimalsByLocation(location)
-      .map(animal => animal.name);
-    if (filteredAnimals.length !== 0) animalsPerLocation[location] = filteredAnimals;
-  });
-  return animalsPerLocation;
-}
-function retrieveAnimalsPerLocationsWithName(locations, sorted, sex) {
-  const animalsPerLocation = {};
-  locations.forEach(location => {
-    const filteredAnimals = retrieveFilteredAnimalsByLocation(location)
-    .map(animal => {
-      const animalName = animal.name;
-      const residents = animal.residents
-      .filter(resident => {
-        const needFiltering = sex !== undefined;
-        // return needFiltering ? resident.sex === sex : true;
-        let retorno;
-        if (needFiltering) {
-          retorno = resident.sex === sex;
-        } else {
-          retorno = true;
-        }
-        return retorno;
-      })
-      .map(resident => resident.name);
-      if (sorted) residents.sort();
-      return { [animalName]: residents };
-    });
-    if (filteredAnimals.length !== 0) animalsPerLocation[location] = filteredAnimals;
-  });
-  return animalsPerLocation;
-}
-
 function animalMap(options) {
-  const locations = retrieveAvailableLocations();
-  if (!options) return retrieveAnimalsPerLocation(locations);
-  const { includeNames = false, sorted = false, sex } = options;
-  let retorno;
-  if (includeNames) {
-    retorno = retrieveAnimalsPerLocationsWithName(locations, sorted, sex);
-  } else {
-    retorno = retrieveAnimalsPerLocation(locations);
-  }
-  return retorno;
+  // seu código aqui
 }
 
 function transformDay(objectParam) {
@@ -167,20 +110,29 @@ function schedule(dayName) {
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
-
-function increasePrices(percentage) {
-  const result = {};
-  if (percentage != undefined) {
-    Object.entries(prices).forEach((price) => {
-      result[price[0]] = Math.round((((price[1] * percentage) / 100) + price[1]) * 100) / 100;
-      console.log(result);
-    });
+  const animalId = employees.find(employee => employee.id === id).responsibleFor[0];
+  const arrayOfAnimalObj = (animals.find(animal => animal.id === animalId).residents);
+  let result = arrayOfAnimalObj.reduce((previousAnimal, actualAnimal) => {
+    if(actualAnimal.age > previousAnimal.age) {
+      previousAnimal = actualAnimal;
+    }
+    return previousAnimal;
+  })
+  result = Object.values(result);
   return result;
 }
 
-console.log(increasePrices(50));
+function increasePrices(percentage) {
+  percentage /= 100;
+  if (percentage) {
+    Object.entries(prices).forEach( ([age, price]) => {
+      const addPercent = percentage * price;
+      let newPrice = Math.round((price + addPercent) * 100);
+      newPrice /= 100;
+      prices[age] = newPrice;
+    });
+  };
+}
 
 function employeeCoverage(idOrName) {
   // seu código aqui
