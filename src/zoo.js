01f6309d98ef120638edb150d4411f25f05008e2
Lets <letsferreiratx@gmail.com>
2020-10-26 17:36:00 -0300

    animalCount ok

--- src/zoo.js
+++ src/zoo.js
@@ -37,16 +37,38 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
-  return employees.forEach(employee => employee).managers.some(manager => manager === id)
+  return employees.some(employee => employee.managers.some(manager => manager === id))
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  employees.push({ id: id, firstName: firstName, lastName: lastName, managers: [...managers], responsibleFor: [...responsibleFor]});
+}
+// addEmployee('4141da1c-a6ed-4cf7-90c4-99c657ba4ef3', 'Jane', 'Doe',
+//       [
+//         '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
+//         'a67a36ee-3765-4c74-8e0f-13f881f6588a',
+//       ],
+//       [
+//         'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
+//         '210fcd23-aa7b-4975-91b7-0230ebb27b99',
+//       ]);
+//       console.log(employees)
+
+function countAnimal(specie){
+  const animalData = animals.filter(animal => animal.name === specie)
+  const num = animalData[0].residents.length
+  return {[specie]: num}
+
 }
 
 function animalCount(species) {
   // seu código aqui
+  if(species === undefined) return animals.reduce((acc, crr) => Object.assign(acc, countAnimal(crr.name)) ,{})
+  return Object.values(countAnimal(species))[0];
+
 }
+
 
 function entryCalculator(entrants) {
   // seu código aqui
