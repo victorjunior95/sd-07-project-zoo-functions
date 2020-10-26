247376c875a74c39e19b8492c0548b195bfbac82
wellington <profwellington17@gmail.com>
2020-10-25 23:11:32 -0300

    fix3 requisito 7

--- src/zoo.js
+++ src/zoo.js
@@ -42,7 +42,7 @@ function isManager(id) {
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   function addEmployee(id, firstName, lastName, managers = [], responsibleFor = [])
-    data.employees.push({ id, firstName, lastName, managers, responsibleFor });
+    data.employees.push({id, firstName, lastName, managers, responsibleFor});
 }
 
 function animalCount(species) {
