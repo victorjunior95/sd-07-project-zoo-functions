ad3cb08b435b6a2f70eeed0e019b5ed3ed3874ac
v-icto-r <victor130496@gmail.com>
2020-10-25 21:59:17 -0400

    correção Requisito 01

--- src/zoo.js
+++ src/zoo.js
@@ -10,7 +10,7 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-const { animals, employees } = require('./data');
+const { animals } = require('./data');
 
 function animalsByIds(...ids) {
   return animals.filter(animal => ids.includes(animal.id));
