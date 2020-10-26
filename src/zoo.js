c262ce119d299b728192bf63cb3f80fe10bbe09d
mayconfelipes <mayconfelipe.dev@gmail.com>
2020-10-26 13:09:14 -0300

    Requisito 6. Go Trybe!

--- src/zoo.js
+++ src/zoo.js
@@ -42,8 +42,14 @@ function isManager(id) {
   );
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return data.employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
