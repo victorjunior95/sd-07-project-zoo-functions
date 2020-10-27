f855321c0da32d8da98cc58fde773ef38064ba73
v-icto-r <victor130496@gmail.com>
2020-10-26 22:44:54 -0300

    Requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -77,7 +77,19 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const scheduleObj = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+  const scheduleDay = {};
+  if (!dayName) return scheduleObj;
+  scheduleDay[dayName] = scheduleObj[dayName];
+  return scheduleDay;
 }
 
 function oldestFromFirstSpecies(id) {
