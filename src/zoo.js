76fa6dd1e47479f6d1efa88d1f40690cf85a8eb1
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-10-27 01:39:11 -0300

    Quinto requisito

--- src/zoo.js
+++ src/zoo.js
@@ -36,7 +36,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(({ managers }) => managers.some(idManage => id === idManage));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
