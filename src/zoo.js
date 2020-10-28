5b509e318f495a9abbeae5f1b30598b4056d2725
Geziel <geziel.fernandez@pucp.pe>
2020-10-28 09:45:16 -0300

    let's see if it works

--- src/zoo.js
+++ src/zoo.js
@@ -7,16 +7,22 @@ eslint no-unused-vars: [
     "varsIgnorePattern": "data"
   }
 ]
-*//
+*/
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+  if (!ids) return [];
+  // return ids.map((id) => {
+  //   [data.animals.id];
+  // })
+  return ids.map(id => data.animals.find(animal => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+
 }
 
 function employeeByName(employeeName) {
@@ -43,10 +49,30 @@ function entryCalculator(entrants) {
   // seu código aqui
 }
 
-function animalMap(options) {
-  // seu código aqui
+/////////////////////////////////////////
+function retrieveAvailableLocations() {
+  return ['NE', 'E', 'NW', 'SW', 'SE'];
 }
 
+function retrieveAnimalsPerLocation(locations) {
+  const animalsPerLocation = {};
+
+  locations.forEach((location) => {
+    const filteredAnimals = animals
+    .filter((animal) => animal.location === location ).map((animal) => animal.name);
+    console.log(`Localização: ${location}`);
+    console.log(filteredAnimals);
+  });
+}
+
+function animalMap(options) {
+  const locations = retrieveAvailableLocations();
+
+  if (!options)
+    return retrieveAnimalsPerLocation(locations);
+}
+
+//////////////////////////////////////////
 function schedule(dayName) {
   // seu código aqui
 }
