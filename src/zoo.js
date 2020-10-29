eae0281e112812691877d647069a808edd81ea8e
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-10-29 06:35:28 -0400

    corrigida funcao animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -12,21 +12,19 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  if(ids.length === 0) {
+  if (ids.length === 0) {
     return [];
   }
   let find = [];
   ids.forEach((id) => {
     data.animals.find((element) => {
-      if(element.id === id) {
+      if (element.id === id) {
         find.push(element);
       }
     });
   });
   return find;
 }
-
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
