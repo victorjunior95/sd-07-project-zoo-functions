04c2377a8ec12febf4edc24a67a6db2cb0d86e84
Geziel <geziel.fernandez@pucp.pe>
2020-11-07 00:42:19 -0300

    6th requirement

--- src/zoo.js
+++ src/zoo.js
@@ -34,8 +34,8 @@ function isManager(id) {
   return data.employees.some(({ managers }) => managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
