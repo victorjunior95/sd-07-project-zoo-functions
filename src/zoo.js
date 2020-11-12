d0663f580c80f5b8e1e962fcfeb1d96764179948
Victor Junior <victorjr.muchacho@gmail.com>
2020-11-12 11:49:04 -0300

    Requisito 1 com ajuste de CC

--- src/zoo.js
+++ src/zoo.js
@@ -10,40 +10,14 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-// data é um {objeto de 4 chaves - animals: é um [array de {9 objects}], employees: [{8 objects}], hours:{object de 7 chaves}, prices: {object de 3 chaves}}
-//data = {
-//   animals: [
-//    {9 especies, 5 chaves}
-//   ], 
-//   employees: [
-//    {8 objects, 5 chaves}
-//   ], 
-//   hours:{7 chaves}, 
-//   prices: {3 chaves}
-// }
 
-// let arrayOfIds = [];
-// const animalsById = data.animals.forEach( animal => { console.log(Object.values(animal)) }); // array de 5(id de cada animal em string)
-
-// console.log(animalsData); 
-
-// console.log(typeof animalsData);
-const {animals, employees, prices, hours} = data; // object destructuring
-// console.log(animals); // retorna um array com os objetos
+const { animals } = data; // object destructuring [, employees, prices, hours]
 
 function animalsByIds(...ids) {
   const listAnimals = [];
-  ids.forEach((id) => listAnimals.push(animals.filter(animal => animal.id === id)))
+  ids.forEach(id => listAnimals.push(animals.filter(animal => animal.id === id)))
   return listAnimals;
-
-  // const animalsData = Object.values(data.animals);
-  // return animalsData.filter( animal => animal.id === ids)
-  // // 1. retornar array vazio
-  // 2. recuperar a especie, em array, especificada no parâmetro (id)
-  // 3. msm q o '2.', mas em caso de mais de um id
-  /* forEach */ 
 }
-// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
