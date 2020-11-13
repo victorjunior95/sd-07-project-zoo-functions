a90f38a8b4f144db580be48c633770a0f7d3cb80
Veruska Bittencourt <sousaveruska@gmail.com>
2020-11-13 17:23:49 -0300

    Primeiro Requisito

--- src/zoo.js
+++ src/zoo.js
@@ -13,7 +13,10 @@ const data = require('./data');
 
 function animalsByIds(ids) {
   // seu código aqui
-}
+  if (ids.length === 0) return [];
+  return animals.filter((animal, index) => animal.id === ids[index]);
+}	
+
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
