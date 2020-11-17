8eb24c9eb8558ef7a8e71787a1f3ab431fc510d8
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-11-17 19:16:56 -0300

    feito ate o 8

--- src/zoo.js
+++ src/zoo.js
@@ -38,9 +38,36 @@ function isManager(id) {
   return employees.some(employee => employee.managers.includes(id));
 }
 
-//   // seu código aqui
-// }
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = { id, firstName, lastName, managers, responsibleFor };
+  data.employees.push(newEmployee);
+}
 
+function animalCount(species) {
+ 
+  if (species === undefined) {
+    const namesSpecies = {};
+    animals.forEach((animal) => {
+      namesSpecies[animal.name] = animal.residents.length;
+    });
+
+    return namesSpecies;
+  }
+  const findAnimal = animals.find(animal => animal.name === species);
+  return findAnimal.residents.length;
+}
+
+function entryCalculator(entrants) {
+  if (entrants === undefined || entrants === {}) return 0;
+
+  const keys = Object.keys(entrants);
+  let count = 0;
+  keys.forEach((age) => {
+    count += entrants[age] * prices[age];
+  });
+
+  return count;
+}
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
 // }
