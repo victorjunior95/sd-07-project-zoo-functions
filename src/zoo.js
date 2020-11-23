17562338147acc048f1d76b93e475d346196835b
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2020-11-23 16:41:13 -0300

    teste

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,12 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require("./data");
+const { animals, employees } = require('./data');
+const data = require('./data');
 
 function animalsByIds(ids) {
   const animals = [];
-  ids.forEach((id) => animals.push(animals.find((animal) => animal.id === id)));
+  ids.forEach(id => animals.push(animals.find((animal) => animal.id === id)));
 }
 
 function animalsOlderThan(animal, age) {
